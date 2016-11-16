2b308953285b5745c3b598daf5607a18bb963885
Jérémy Lal <kapouer@melix.org>
2016-11-16 15:29:59 +0100

    Move files in other modules

--- packages/core/routes/files/media.js
+++
@@ -1,9 +0,0 @@
-var sharpie = require('sharpie');
-var rewrite = require('express-urlrewrite');
-
-exports.route = function(app, api, config) {
-	// case when parameter is passed as query.url
-	app.get('/media/image', sharpie(config.sharpie));
-	app.get('/media/:type/:filename', rewrite('/uploads/:filename'));
-};
-
