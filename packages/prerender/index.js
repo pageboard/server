7b7a5046bbb4dd609f4b0e23692b14b09ad3eb1d
Jérémy Lal <kapouer@melix.org>
2016-11-22 18:48:09 +0100

    Use All

--- packages/prerender/index.js
+++ packages/prerender/index.js
@@ -2,20 +2,20 @@ var dom = require('express-dom');
 var expressHref = require('express-href');
 var Path = require('path');
 
-module.exports = function(config) {
-	if (!config.prerender) config.prerender = {};
+module.exports = function(opt) {
+	if (!opt.prerender) opt.prerender = {};
 
 	Object.assign(dom.settings, {
 		stall: 20000,
 		allow: "same-origin",
-		cacheDir: Path.join(config.dirs.cache, "prerender")
-	}, config.prerender);
+		cacheDir: Path.join(opt.dirs.cache, "prerender")
+	}, opt.prerender);
 
 	Object.assign(dom.pool, {
 		max: 8
-	}, config.prerender.pool);
+	}, opt.prerender.pool);
 
-	if (config.prerender.pool) delete dom.settings.pool;
+	if (opt.prerender.pool) delete dom.settings.pool;
 
 	dom.clear();
 
@@ -24,10 +24,10 @@ module.exports = function(config) {
 	};
 };
 
-function init(app, modules, config) {
-	app.set('views', config.statics.root);
-	expressHref(app);
+function init(All) {
+	All.app.set('views', All.opt.statics.root);
+	expressHref(All.app);
 	// the router is universal and available in pageboard-read
-	app.get('*', dom('router').load());
+	All.app.get('*', dom('router').load());
 };
 
