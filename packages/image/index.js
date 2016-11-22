9d5ab4f68a90d71747890831237a8a382c648c96
Jérémy Lal <kapouer@melix.org>
2016-11-22 18:49:07 +0100

    Use All

--- packages/image/index.js
+++ packages/image/index.js
@@ -1,13 +1,13 @@
 var sharpie = require('sharpie');
 var rewrite = require('express-urlrewrite');
 
-module.exports = function(config) {
+module.exports = function(opt) {
 	return {file: init};
 };
 
-function init(app, modules, config) {
+function init(All) {
 	// case when parameter is passed as query.url
-	app.get('/media/image', sharpie(config.sharpie));
-	app.get('/media/:type/:filename', rewrite('/uploads/:filename'));
+	All.app.get('/media/image', sharpie(All.opt.sharpie));
+	All.app.get('/media/:type/:filename', rewrite('/uploads/:filename'));
 }
 
