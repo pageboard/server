53f3a64d7b1fb6d608b71439474c843bf7b7e228
Jérémy Lal <kapouer@melix.org>
2016-11-24 00:42:54 +0100

    Some fixes for page api

--- packages/api/services/page.js
+++ packages/api/services/page.js
@@ -6,27 +6,40 @@ exports = module.exports = function(config) {
 };
 
 function init(All) {
+	All.app.get('/api/page', function(req, res, next) {
+		exports.get(req.query).then(function(page) {
+			res.send(page);
+		}).catch(next);
+	});
 	All.app.post('/api/page', function(req, res, next) {
 		exports.create(req.body).then(function(page) {
+			res.sendStatus(200);
+		}).catch(next);
+	});
+	All.app.delete('/api/page', function(req, res, next) {
+		exports.remove(req.query).then(function(page) {
 			res.sendStatus(200);
 		}).catch(next);
 	});
 }
 
 exports.get = function(data) {
-	return All.Block.query().where({
-		url: data.url,
-		type: 'page',
-		mime: 'text/html'
+	if (!data.site) return Promise.reject(new HttpError.BadRequest("Missing site"));
+	if (!data.url) return Promise.reject(new HttpError.BadRequest("Missing url"));
+	return All.Block.query().select('block.*').where({
+		'block.url': data.url,
+		'block.type': 'page',
+		'block.mime': 'text/html'
 	})
 	.eager('children.^')
 	.joinRelation('parents').where({
-		type: 'site',
+		'parents.type': 'site',
 		'parents.url': data.site
 	}).first();
 };
 
 exports.create = function(data) {
+	if (!data.site) return Promise.reject(new HttpError.BadRequest("Missing site"));
 	data = Object.assign({
 		type: 'page',
 		mime: 'text/html'
@@ -44,9 +57,8 @@ exports.create = function(data) {
 
 
 exports.remove = function(data) {
-	if (!data.url || !data.site) {
-		return Promise.reject(new HttpError.BadRequest("Missing url or site"));
-	}
+	if (!data.site) return Promise.reject(new HttpError.BadRequest("Missing site"));
+	if (!data.url) return Promise.reject(new HttpError.BadRequest("Missing url"));
 
 	return All.Block.query().del().where(url, data.url)
 		.joinRelation('parents').where({
