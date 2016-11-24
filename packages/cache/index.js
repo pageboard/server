35db01d01e7138e66c9563d90c49c53b2570df3e
Jérémy Lal <kapouer@melix.org>
2016-11-24 14:38:33 +0100

    Remove scope initialization from here

--- packages/cache/index.js
+++ packages/cache/index.js
@@ -1,6 +1,5 @@
 exports = module.exports = function(opt) {
 	exports.tag = require('upcache/tag');
-	exports.scope = require('upcache/scope')(opt.scope);
 	exports.vary = require('upcache/vary');
 };
 
