60c26eccb6969956929353a49590938f3848f7be
Jérémy Lal <kapouer@melix.org>
2016-11-29 15:31:59 +0100

    Basic template loading

--- packages/read/public/js/pageboard.js
+++ packages/read/public/js/pageboard.js
@@ -2,6 +2,11 @@ Page.route(function(state) {
 	return GET('/api/page', {
 		url: state.pathname
 	}).then(function(page) {
-		console.log(page);
+		return GET({
+			url: page.template,
+			type: 'html'
+		});
+	}).then(function(doc) {
+		state.document = doc;
 	});
 });
