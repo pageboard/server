b410c083afad424ca1e427e0de5c16f73f128e73
Jérémy Lal <kapouer@melix.org>
2017-02-02 09:38:35 +0100

    Basic reader

--- packages/read/public/js/pageboard.js
+++ packages/read/public/js/pageboard.js
@@ -1,5 +1,19 @@
 Page.build(function(state) {
 	var page = state.data.page;
-	if (!page) throw new Error("Missing page object");
+	var blocks = buildBlocksMap(page);
+	var pagecut = new Pagecut.Viewer();
+	var dom = Pagecut.modules.id.from(pagecut, page.content.body, blocks);
+	document.body.innerHTML = ""; // could use diff-dom or morphdom here
+	document.body.appendChild(document.adoptNode(dom));
 
+	function buildBlocksMap(block, blocks) {
+		if (!blocks) blocks = [];
+		Pagecut.modules.id.set(block); // seed cache as well
+		if (block.children) block.children.forEach(function(item) {
+			blocks[item.id] = item;
+			buildBlocksMap(item, blocks);
+		});
+		return blocks;
+	}
 });
+
---
+++ packages/read/public/js/pagecut-id.js
@@ -0,0 +1,1 @@
+../../node_modules/pagecut/dist/id.js
