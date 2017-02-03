e06be57a1176494b42471deef19e42d75b15390b
Jérémy Lal <kapouer@melix.org>
2017-02-03 11:01:42 +0100

    Install route for read

--- packages/read/index.js
+++ packages/read/index.js
@@ -1,3 +1,10 @@
 module.exports = function(opt) {
 	opt.statics.mounts.push(__dirname + '/public');
+	return {
+		view: init
+	};
 };
+
+function init(All) {
+	All.app.get('*', All.dom('read').load());
+}
