fe993b215c2e568463a4a96e91f083203d25062e
Jérémy Lal <kapouer@melix.org>
2017-03-06 12:16:27 +0100

    Polyfill HTML imports when prerendering (only)

--- packages/read/index.js
+++ packages/read/index.js
@@ -6,5 +6,18 @@ module.exports = function(opt) {
 };
 
 function init(All) {
-	All.app.get('*', All.dom('read').load());
+	// TODO use opt.prerender to configure dom plugins
+	// TODO expose route for preload and route for load,
+	// the route for load will use the preload route as source (view helper can pipe http requests)
+	All.app.get('*', All.dom('read').load(prerenderPolyfillImports));
+}
+
+/*
+* express-dom load plugin that injects html import polyfill
+*/
+function prerenderPolyfillImports(page, settings) {
+	if (!module.htmlImportPolyfill) {
+		module.htmlImportPolyfill = require('fs').readFileSync(require.resolve('@webcomponents/html-imports'));
+	}
+	settings.load.scripts.unshift(module.htmlImportPolyfill);
 }
