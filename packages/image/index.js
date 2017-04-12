151d3ebb62395c09b7216c3ec73332ab303ab325
Jérémy Lal <kapouer@melix.org>
2017-04-12 10:36:09 +0200

    Only resize local images on the fly, expose image.thumbnail api

--- packages/image/index.js
+++ packages/image/index.js
@@ -1,13 +1,68 @@
 var sharpie = require('sharpie');
-var rewrite = require('express-urlrewrite');
+var BufferList = require('bl');
+var DataUri = require('datauri');
 
-module.exports = function(opt) {
-	return {file: init};
+var thumbnailer;
+
+exports = module.exports = function(opt) {
+	if (!opt.image) opt.image = {};
+	if (!opt.image.dir) opt.image.dir = opt.upload && opt.upload.dir || 'public/uploads';
+
+	thumbnailer = sharpie(Object.assign({
+		rs: 'h:128,max',
+		q: '75',
+		bg: 'white',
+		flatten: true,
+		hostnames: true
+	}, All.opt.thumbnail));
+
+	return {
+		name: image,
+		file: init
+	};
 };
 
+// deux cas potentiels
+// - un fichier uploadé est utilisé comme une image, /public/uploads/file.jpg?rs=xxx
+// - un fichier distant est utilisé comme une image. On ne peut pas redimensionner.
+// - fabriquer un thumbnail à partir d'une image distante
+
 function init(All) {
-	// case when parameter is passed as query.url
-	All.app.get('/media/image', sharpie(All.opt.sharpie));
-	All.app.get('/media/:type/:filename', rewrite('/uploads/:filename'));
+	All.app.get(`:url(/${All.opt.image.dir}/.*)`, sharpie(All.opt.image));
 }
 
+exports.thumbnail = function(url, query) {
+	return new Promise(function(resolve, reject) {
+		var status, mime;
+		var req = {
+			params: {url: url},
+			query: query || {},
+			get: function() { return ''; }
+		};
+
+		var res = BufferList(function(err, data) {
+			if (err) return reject(err);
+			var dtu = new DataUri();
+			dtu.format('.' + mime.split('/').pop(), data);
+			resolve(dtu.content);
+		});
+		res.setHeader = function(name, value) {
+			if (name.toLowerCase() == 'content-type') {
+				mime = value;
+			}
+		};
+		res.get = function() {};
+		res.type = function() { return res; };
+		res.status = function(code) {
+			status = code;
+			return res;
+		};
+		res.send = function(txt) {
+			if (status != 200) reject(HttpError(status, txt));
+			return res;
+		};
+		thumbnailer(req, res, function(err) {
+			reject(new Error(err));
+		});
+	});
+};
