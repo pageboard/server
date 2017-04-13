b42f144b02a701e3d71313cfbe6de72cb96abb9d
Jérémy Lal <kapouer@melix.org>
2017-04-13 14:17:36 +0200

    Move read script to its own file

---
+++ packages/read/public/pageboard/read.js
@@ -0,0 +1,77 @@
+Page.route(function(state) {
+	return GET('/api/page', {
+		url: state.pathname
+	}).then(function(page) {
+
+		// page children are
+		// 1) all single blocks that are on this page
+		// 2) all shared blocks that are on this page
+
+		// single blocks do not have other relations stored in db
+		// shared blocks are related to all the blocks they contain, like a page do
+
+		// the relations of inclusions between blocks is only stored in the html
+		// only the relations needed to rebuild pages or shared blocks are stored in db
+
+		// as a consequence, a shared block cannot contain shared blocks
+		// and there is no such thing as a shared page
+		// however, when copying a block, the shared blocks are kept shared
+
+		var viewer = Pagecut.viewerInstance = new Pagecut.Viewer();
+
+		viewer.modules.id.store = buildBlocksMap(page);
+
+		var stylesheets = collectStylesheets({}, document);
+
+		return viewer.modules.id.from(page).then(function(fragment) {
+			if (fragment.nodeName != "BODY") throw new Error("Page renderer should fill document and return body");
+			state.document = fragment.ownerDocument.cloneNode(true);
+			setStylesheets(stylesheets, state.document);
+		});
+	}).catch(function(err) {
+		console.error(err);
+		var params = {
+			code: err.statusCode || err.code || 500,
+			message: err.message || err.toString()
+		};
+		/*
+		document.location = Page.format({
+			pathname: '/error',
+			query: params
+		});
+		*/
+		document.body.innerHTML = '<h1>Error' + params.code + '</h1>' +
+			'<p>' + params.message + '</p>';
+	});
+
+	function buildBlocksMap(block, blocks) {
+		if (!blocks) blocks = {};
+		blocks[block.id] = block;
+		if (block.children) block.children.forEach(function(item) {
+			buildBlocksMap(item, blocks);
+		});
+		return blocks;
+	}
+
+	function collectStylesheets(sheets, doc) {
+		var nodes = Array.from(doc.querySelectorAll('link[rel="import"],link[rel="stylesheet"]'));
+		nodes.forEach(function(node) {
+			if (node.import) collectStylesheets(sheets, node.import);
+			else sheets[node.href] = node.getAttribute('href');
+		});
+		return sheets;
+	}
+
+	function setStylesheets(map, doc) {
+		var pivot = doc.head.querySelector('script');
+		var href, sheet;
+		for (var k in map) {
+			href = map[k];
+			sheet = doc.createElement('link');
+			sheet.rel = "stylesheet";
+			sheet.setAttribute('href', href);
+			if (pivot) doc.head.insertBefore(sheet, pivot);
+			else doc.head.appendChild(sheet);
+		}
+	}
+});
