07eac55e8fe6721e0dc39f041d8e1845ef0d2f8d
Jérémy Lal <kapouer@melix.org>
2017-09-27 23:32:06 +0200

    html imports polyfill is no longer needed

--- packages/read/index.js
+++ packages/read/index.js
@@ -1,6 +1,5 @@
 var readFile = require('util').promisify(require('fs').readFile);
 var Path = require('path');
-var htmlImportPolyfill;
 
 module.exports = function(opt) {
 	return {
@@ -13,17 +12,8 @@ function init(All) {
 	// TODO expose route for preload and route for load,
 	// the route for load will use the preload route as source (view helper can pipe http requests)
 
-	return Promise.all([readFile(Path.join(__dirname, 'read/read.html')).then(function(buf) {
-		All.app.get('*', All.dom(buf).load(prerenderPolyfillImports));
-	}), readFile(require.resolve('@webcomponents/html-imports')).then(function(buf) {
-		htmlImportPolyfill = buf;
-	})]);
+	return readFile(Path.join(__dirname, 'read/read.html')).then(function(buf) {
+		All.app.get('*', All.dom(buf).load());
+	});
 }
 
-/*
-* server prerendering need html import polyfill
-*/
-
-function prerenderPolyfillImports(page, settings) {
-	settings.load.scripts.unshift(htmlImportPolyfill);
-}
