2e896d7919bfc43873fc5991b4077635bd036a1d
Jérémy Lal <kapouer@melix.org>
2017-11-28 13:19:38 +0100

    Quick and dirty html / markdown email

--- packages/mail/express-dom-email.js
+++ packages/mail/express-dom-email.js
@@ -22,12 +22,31 @@ function mailPlugin(page, settings, request, response) {
 //	settings.scripts.push(irBuf);
 	page.when('idle', function() {
 		return page.run(function(done) {
+			var frag = document.createElement("div");
+			var md = Array.from(document.querySelectorAll('h1,h2,h3,h4,h5,h6,a,p,img')).map(function(node) {
+				node.removeAttribute('block-id');
+				node.removeAttribute('block-type');
+				node.removeAttribute('block-content');
+				node.removeAttribute('class');
+				var tag = node.nodeName.toLowerCase();
+				if (tag != "br" && tag != "img" && node.childNodes.length == 0) return;
+				frag.appendChild(node.cloneNode(true));
+				var heading = tag.match(/h(\d)/);
+				if (heading) {
+					return Array(parseInt(heading[1]) + 1).join('#') + node.innerText;
+				}
+				if (tag == "a") return `[${node.innerText}](${node.href})`;
+				if (tag == "p") return `\n${node.innerText}\n`;
+				if (tag == "img") return `![${node.alt}](${node.src})`;
+			});
 //			inlineresources.inlineReferences(document, {}).then(function (errors) {
 				done(null, {
 //					errors: errors,
 					title: document.title,
-					html: `<html>${document.body.outerHTML}</html>`,
-					text: document.body.innerText
+					html: frag.innerHTML,
+					text: md.filter(function(line) {
+						return !!line;
+					}).join('\n')
 				});
 //			});
 		}).then(function(obj) {
