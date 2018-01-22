c32de89ff50913b52930b083ce9cd5c835354f51
Jérémy Lal <kapouer@melix.org>
2018-01-22 15:23:14 +0100

    Automatic Content-Security-Policy

---
+++ packages/prerender/plugins/csp.js
@@ -0,0 +1,24 @@
+var csp = require("content-security-policy-builder");
+
+module.exports = function cspPlugin(page, settings, request, response) {
+	page.when('idle', function() {
+		page.run(function() {
+			var hosts = {};
+			Array.from(
+				document.querySelectorAll('script[src]')
+			).forEach(function(node) {
+				var obj = Page.parse(node.src);
+				if (obj.hostname) hosts[obj.hostname] = true;
+			});
+			return Object.keys(hosts);
+		}).then(function(hosts) {
+			response.setHeader('Content-Secutiry-Policy', csp({
+				directives: {
+					defaultSrc: ["'self'"],
+					scriptSrc: hosts
+				}
+			}));
+		});
+	});
+};
+
