68af638d9e7d9b999a2df445ba2de9388940f567
Jérémy Lal <kapouer@melix.org>
2018-02-23 09:31:38 +0100

    Setup window.Pageboard.bindings and HTMLCustomElement

--- packages/read/read/pageboard.js
+++ packages/read/read/pageboard.js
@@ -1,5 +1,9 @@
+if (!window.Pageboard) window.Pageboard = {};
+if (!window.Pageboard.bindings) window.Pageboard.bindings = {};
+
 // https://github.com/WebReflection/document-register-element#skipping-the-caveat-through-extends
 class HTMLCustomElement extends HTMLElement {
 	constructor(_) { return (_ = super(_)).init(), _; }
 	init() { /* override as you like */ }
 }
+
