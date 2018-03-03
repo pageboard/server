0617995596d8cebb6fdaba08cdc5e97da73eebfc
Jérémy Lal <kapouer@melix.org>
2018-03-03 12:48:02 +0100

    site.version, site.env, and give some docs

--- packages/api/elements/site.js
+++ packages/api/elements/site.js
@@ -2,13 +2,25 @@ Pageboard.elements.site = {
 	required: ['domain'],
 	properties : {
 		title: {
+			title: 'Site title',
 			type: 'string' // the site public name
 		},
 		domain: {
+			title: 'Domain name',
 			type: 'string'
 		},
 		module: {
+			title: 'Module name',
 			type: 'string'
+		},
+		version: {
+			title: 'Module version',
+			type: 'string'
+		},
+		env: {
+			title: 'Production',
+			type: 'boolean',
+			default: false
 		}
 	}
 };
