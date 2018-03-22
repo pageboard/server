ed12da41f0b8817399db09840b34d6bd84b34e1b
Jérémy Lal <kapouer@melix.org>
2018-03-22 12:24:48 +0100

    json ld for google button

--- packages/mail/express-dom-email.js
+++ packages/mail/express-dom-email.js
@@ -29,7 +29,7 @@ function mailPlugin(page, settings, request, response) {
 //	settings.scripts.push(irBuf);
 	page.when('idle', function() {
 		return page.run(function(done) {
-			var frag = document.createElement("div");
+			var fragBody = document.createElement("div");
 			var md = Array.from(document.querySelectorAll('h1,h2,h3,h4,h5,h6,a,p,img')).map(function(node) {
 				node.removeAttribute('block-id');
 				node.removeAttribute('block-type');
@@ -37,7 +37,7 @@ function mailPlugin(page, settings, request, response) {
 				node.removeAttribute('class');
 				var tag = node.nodeName.toLowerCase();
 				if (tag != "br" && tag != "img" && node.childNodes.length == 0) return;
-				frag.appendChild(node.cloneNode(true));
+				fragBody.appendChild(node.cloneNode(true));
 				var heading = tag.match(/h(\d)/);
 				if (heading) {
 					return Array(parseInt(heading[1]) + 1).join('#') + node.innerText;
@@ -46,11 +46,17 @@ function mailPlugin(page, settings, request, response) {
 				if (tag == "p") return `\n${node.innerText}\n`;
 				if (tag == "img") return `![${node.alt}](${node.src})`;
 			});
+			var fragHead = document.createElement("div");
+			var jsonld = document.querySelectorAll('script[type="application/ld+json"]');
+			Array.from(jsonld).forEach(function(node) {
+				node.removeAttribute('id');
+				fragHead.appendChild(node.cloneNode(true));
+			});
 //			inlineresources.inlineReferences(document, {}).then(function (errors) {
 				done(null, {
 //					errors: errors,
 					title: document.title,
-					html: frag.innerHTML,
+					html: `<html><body>${fragHead.innerHTML} ${fragBody.innerHTML}</body></html>`,
 					text: md.filter(function(line) {
 						return !!line;
 					}).join('\n')
