b0adb4dc514223687205269a3d46832080328125
Jérémy Lal <kapouer@melix.org>
2018-04-08 03:07:41 +0200

    Block: use class methods syntax, introduce patchObject, whereObject

--- packages/api/models/block.js
+++ packages/api/models/block.js
@@ -220,19 +220,44 @@ Block.genId = function(length) {
 	});
 };
 
-class BlockQueryBuilder extends QueryBuilder {}
+class BlockQueryBuilder extends QueryBuilder {
+	whereSite(siteId) {
+		return this.joinRelation('parents')
+			.where('parents.type', 'site')
+			.where('parents.id', siteId);
+	}
+	whereJsonText(a) {
+		var args = Array.from(arguments).slice(1);
+		args.unshift(ref(a).castText());
+		return this.where.apply(this, args);
+	}
+	patchObject(obj) {
+		var refs = asPaths(obj, {}, "", true);
+		this.addOperation(this._patchOperationFactory(this), refs);
+		return this;
+	}
+	whereObject(obj) {
+		var table = this.tableRefFor(this.modelClass());
+		var refs = asPaths(obj, {}, table + '.', true);
+		for (var k in refs) {
+			this.where(ref(k).castText(), Array.isArray(refs[k]) ? 'IN' : '=', refs[k]);
+		}
+		return this;
+	}
+}
 
-BlockQueryBuilder.prototype.whereSite = function(siteId) {
-	return this.joinRelation('parents')
-		.where('parents.type', 'site')
-		.where('parents.id', siteId);
-};
-
-BlockQueryBuilder.prototype.whereJsonText = function(a) {
-	var args = Array.from(arguments).slice(1);
-	args.unshift(ref(a).castText());
-	return this.where.apply(this, args);
-};
+function asPaths(obj, ret, pre, first) {
+	Object.keys(obj).forEach(function(key) {
+		var val = obj[key];
+		var cur = `${pre || ""}${key}`;
+		if (Array.isArray(val) || typeof val != "object") {
+			ret[cur] = val;
+		} else if (typeof val == "object") {
+			asPaths(val, ret, cur + (first ? ':' : '.'));
+		}
+	});
+	return ret;
+}
 
 Block.QueryBuilder = BlockQueryBuilder;
 
