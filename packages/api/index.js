b9389ce8677b0bf2e6d6953d992d7f3fc73ac8de
Jérémy Lal <kapouer@melix.org>
2018-04-20 13:57:51 +0200

    Split in several files

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,10 +1,15 @@
 var objection = require('objection');
-var AjvKeywords = require('ajv-keywords');
-var bodyParserJson = require('body-parser').json();
-var schedule = require('node-schedule');
+var knex = require('knex');
 
 var ajv = require('ajv');
+var AjvKeywords = require('ajv-keywords');
 var ajvMetaSchema = require('ajv/lib/refs/json-schema-draft-06.json');
+
+var bodyParserJson = require('body-parser').json();
+
+var dba = require('./lib/dba');
+var imports = require('./lib/imports');
+
 
 var ajvApiSettings = {
 	$data: true,
@@ -21,21 +26,6 @@ var ajvApiWithDefaults = ajv(Object.assign({}, ajvApiSettings, {
 var ajvApiWithNoDefaults = ajv(Object.assign({}, ajvApiSettings, {
 	useDefaults: false
 })).addMetaSchema(ajvMetaSchema);
-
-var knex = require('knex');
-
-var Path = require('path');
-var pify = require('util').promisify;
-var toSource = require('tosource');
-
-var exec = pify(require('child_process').exec);
-
-var fs = {
-	readFile: pify(require('fs').readFile)
-};
-var vm = require('vm');
-
-var debug = require('debug')('pageboard:api');
 
 exports = module.exports = function(opt) {
 	opt.plugins.unshift(
@@ -62,7 +52,7 @@ exports = module.exports = function(opt) {
 
 function init(All) {
 	var opt = All.opt;
-	var dbOpt = knexConfig(opt);
+	var dbOpt = dba.knexConfig(opt);
 	var knexInst = knex(dbOpt);
 
 	objection.Model.createValidator = function() {
@@ -93,9 +83,12 @@ function init(All) {
 	exports.Block = models.Block;
 	exports.trx = knexInst.transaction.bind(knexInst);
 
-	exports.migrate = migrate.bind(null, knexInst, opt.migrations);
-	exports.seed = seed.bind(null, knexInst, opt.seeds);
-	exports.dump = dumpDb.bind(null, dbOpt.connection, opt);
+	exports.migrate = dba.migrate.bind(null, knexInst, opt.migrations);
+	exports.seed = dba.seed.bind(null, knexInst, opt.seeds);
+	exports.dump = dba.dump.bind(null, dbOpt.connection, opt);
+	exports.gc = dba.gc;
+
+	Object.assign(exports, imports);
 
 	All.app.get('/.api/elements.js',
 		All.cache.tag('share', 'file').for('0s'),
@@ -129,303 +122,3 @@ exports.check = function(fun, data) {
 	}
 };
 
-exports.install = function(site, pkg, All) {
-	var elements = pkg.elements;
-	var directories = pkg.directories;
-	debug("installing", id, elements, directories);
-	var id = site ? site.id : null;
-	var allDirs = id ? All.opt.directories.concat(directories) : directories;
-	var allElts = id ? All.opt.elements.concat(elements) : elements;
-	var elts = {};
-	var names = [];
-	var context = {};
-	return Promise.all(allElts.map(function(path) {
-		return importElements(path, context, elts, names);
-	})).then(function() {
-		var eltsMap = {};
-		names.forEach(function(name) {
-			var elt = Object.assign({}, elts[name]); // drop proxy
-			rewriteElementPaths(name, elt, id, allDirs);
-			eltsMap[name] = elt;
-		});
-		var Block = exports.Block.extendSchema(id, eltsMap);
-		if (id) {
-			pkg.Block = Block;
-			pkg.eltsMap = eltsMap;
-		} else {
-			exports.Block = All.api.Block = Block;
-		}
-	}).catch(function(err) {
-		console.error(err);
-		throw err;
-	});
-};
-
-exports.validate = function(site, pkg) {
-	return Promise.resolve().then(function() {
-		var eltsMap = pkg.eltsMap;
-		var env = site.data.env;
-		var list = Object.keys(eltsMap).map(function(key) {
-			var el = eltsMap[key];
-			if (!el.name) el.name = key;
-			return el;
-		}).sort(function(a, b) {
-			return (a.priority || 0) - (b.priority || 0);
-		});
-		eltsMap.page = Object.assign({}, eltsMap.page);
-
-		var scripts = filter(list, 'scripts');
-		var styles = filter(list, 'stylesheets');
-
-		if (env == "dev" || !pkg.dir || !site.href) {
-			eltsMap.page.scripts = scripts;
-			eltsMap.page.stylesheets = styles;
-			return Promise.resolve();
-		}
-
-		return Promise.all([
-			All.statics.bundle(site, scripts, `scripts.js`),
-			All.statics.bundle(site, styles, `styles.css`)
-		]).then(function(both) {
-			eltsMap.page.scripts = both[0];
-			eltsMap.page.stylesheets = both[1];
-		});
-	}).then(function() {
-		site.constructor = pkg.Block;
-		site.$source = toSource(pkg.eltsMap);
-		delete pkg.eltsMap;
-		delete pkg.Block;
-	});
-};
-
-function filter(elements, prop) {
-	var map = {};
-	var res = [];
-	elements.forEach(function(el) {
-		var list = el[prop];
-		if (!list) return;
-		delete el[prop];
-		if (typeof list == "string") list = [list];
-		var url, prev;
-		for (var i=0; i < list.length; i++) {
-			url = list[i];
-			prev = map[url];
-			if (prev) {
-				if (el.priority != null) {
-					if (prev.priority == null) {
-						// move prev url on top of res
-						res = res.filter(function(lurl) {
-							return lurl != url;
-						});
-					} else if (prev.priority != el.priority) {
-						console.warn(prop, url, "declared in element", el.name, "with priority", el.priority, "is already declared in element", prev.name, "with priority", prev.priority);
-						continue;
-					} else {
-						continue;
-					}
-				} else {
-					continue;
-				}
-			}
-			map[url] = el;
-			res.push(url);
-		}
-	});
-	return res;
-}
-
-
-function getMountPath(eltPath, id, directories) {
-	var mount = directories.find(function(mount) {
-		return eltPath.startsWith(mount.from);
-	});
-	if (!mount) {
-		console.warn(`Warning: element ${eltPath} cannot be mounted`);
-		return;
-	}
-	var basePath = id ? mount.to.replace(id + "/", "") : mount.to;
-	var eltPathname = Path.join(basePath, eltPath.substring(mount.from.length));
-	return Path.dirname(eltPathname);
-}
-
-function rewriteElementPaths(name, elt, id, directories) {
-	['scripts', 'stylesheets', 'resources'].forEach(function(what) {
-		if (elt[what] != null) {
-			elt[what] = elt[what]
-			.map(function(obj) {
-				var dir = getMountPath(obj.base, id, directories);
-				if (!dir) {
-					console.error("Could not find mount path for", obj.base, id);
-					return;
-				}
-				if (obj.path.startsWith('/') || /^(http|https|data):/.test(obj.path)) return obj.path;
-				return Path.join(dir, obj.path);
-			})
-			.filter(x => !!x);
-		} else {
-			delete elt[what];
-		}
-	});
-}
-
-function importElements(path, context, elts, names) {
-	return fs.readFile(path).then(function(buf) {
-		context.path = path;
-		var script = new vm.Script(buf, {
-			filename: path
-		});
-		var sandbox = {
-			Pageboard: {
-				elements: elts
-			}
-		};
-		script.runInNewContext(sandbox, {
-			filename: path,
-			timeout: 1000
-		});
-
-		var elt;
-		for (var name in elts) {
-			elt = elts[name];
-			if (!elt) {
-				console.warn("element", name, "is not defined at", path);
-				continue;
-			}
-			['scripts', 'stylesheets', 'resources'].forEach(function(what) {
-				var list = elt[what];
-				if (!list) return;
-				if (typeof list == "string") list = [list];
-				elt[what] = list.map(function(str) {
-					return {base: path, path: str};
-				});
-			});
-			if (name == "user") {
-				console.warn(`Drop element ${name} at ${path}`);
-				elt = {};
-			} else {
-				names.push(name);
-			}
-			Object.defineProperty(elts, name, {
-				value: new Proxy(elt, new EltProxy(context)),
-				writable: false,
-				enumerable: false,
-				configurable: false
-			});
-		}
-	});
-}
-
-class EltProxy {
-	constructor(context) {
-		this.context = context;
-	}
-	set(elt, name, val) {
-		if (name == "scripts" || name == "stylesheets" || name == "resources") {
-			val = val.map(function(str) {
-				return {
-					path: str,
-					base: this.context.path // this value change with path by design
-				};
-			}, this);
-		}
-		return Reflect.set(elt, name, val);
-	}
-}
-
-function migrate(knex, dirs) {
-	return Promise.all(dirs.map(function(dir) {
-		console.info(` ${dir}`);
-		return knex.migrate.latest({
-			directory: dir
-		}).spread(function(batchNo, list) {
-			if (list.length) return list;
-			return "No migrations run in this directory";
-		});
-	}));
-}
-
-function seed(knex, dirs) {
-	return Promise.all(dirs.map(function(dir) {
-		console.info(` ${dir}`);
-		return knex.seed.run({
-			directory: dir
-		}).spread(function(list) {
-			if (list.length) console.log(" ", list.join("\n "));
-			else console.log("No seed files in", dir);
-		});
-	}));
-}
-
-function dumpDb(conn, opt) {
-	var stamp = (new Date).toISOString().split('.')[0].replace(/[-:]/g, '');
-	var file = Path.join(opt.database.dump.dir, `${opt.name}-${stamp}.dump`);
-	return exec(`pg_dump --format=custom --file=${file} --username=${conn.user} ${conn.database}`, {}).then(function() {
-		return file;
-	});
-}
-
-function knexConfig(config) {
-	if (!process.env.HOME) process.env.HOME = require('passwd-user').sync(process.getuid()).homedir;
-	var dbOpts = Object.assign({}, {
-		url: `postgres://localhost/${opt.name}`
-	}, config.database);
-	delete dbOpts.dump;
-	var parsed = require('url').parse(dbOpts.url, true);
-	delete dbOpts.url;
-	var conn = {};
-	var obj = { connection: conn };
-	if (parsed.host) conn.host = parsed.host;
-	if (parsed.pathname) conn.database = parsed.pathname.substring(1);
-	if (parsed.auth) {
-		var auth = parsed.auth.split(':');
-		conn.user = auth[0];
-		if (auth.length > 1) conn.password = auth[1];
-	}
-	if (parsed.protocol) obj.client = parsed.protocol.slice(0, -1);
-	if (dbOpts.client) {
-		obj.client = dbOpts.client;
-		delete dbOpts.client;
-	}
-	obj.debug = require('debug').enabled('pageboard:sql');
-	if (dbOpts.debug) {
-		obj.debug = dbOpts.debug;
-		delete dbOpts.debug;
-	}
-	Object.assign(conn, dbOpts);
-	return obj;
-}
-
-var gcJob;
-exports.gc = function(All) {
-	var opts = All.opt.gc;
-	if (!opts) opts = All.opt.gc = {};
-	var blockDays = parseInt(opts.block);
-	if (isNaN(blockDays)) blockDays = 1;
-	var hrefDays = parseInt(opts.href);
-	if (isNaN(hrefDays)) hrefDays = 7;
-	opts.block = blockDays;
-	opts.href = hrefDays;
-
-	var interval = Math.max(Math.min(blockDays, hrefDays), 1) * 24 * 60 * 60 * 1000;
-	var jump = gcJob == null;
-	gcJob = schedule.scheduleJob(new Date(Date.now() + interval), exports.gc.bind(null, All));
-	if (jump) return;
-
-	return Promise.all([
-		All.block.gc(blockDays),
-		All.href.gc(hrefDays)
-	]).then(function([blockResult, hrefResult]) {
-		if (blockResult.length) {
-			console.info(`gc: ${blockResult.length} blocks since ${blockDays} days`);
-		}
-		if (hrefResult.length) {
-			console.info(`gc: ${hrefResult.length} hrefs since ${hrefDays} days`);
-		}
-		return Promise.all(hrefResult.map(function(obj) {
-			if (obj.type == "link") return Promise.resolve();
-			return All.upload.gc(obj.site, obj.pathname).catch(function(ex) {
-				console.error("gc error", obj.id, obj.url, ex);
-			});
-		}));
-	});
-};
---
+++ packages/api/lib/imports.js
@@ -0,0 +1,209 @@
+var Path = require('path');
+var pify = require('util').promisify;
+var toSource = require('tosource');
+var exec = pify(require('child_process').exec);
+
+var fs = {
+	readFile: pify(require('fs').readFile)
+};
+var vm = require('vm');
+var debug = require('debug')('pageboard:api');
+
+exports.install = function(site, pkg, All) {
+	var elements = pkg.elements;
+	var directories = pkg.directories;
+	debug("installing", id, elements, directories);
+	var id = site ? site.id : null;
+	var allDirs = id ? All.opt.directories.concat(directories) : directories;
+	var allElts = id ? All.opt.elements.concat(elements) : elements;
+	var elts = {};
+	var names = [];
+	var context = {};
+	return Promise.all(allElts.map(function(path) {
+		return importElements(path, context, elts, names);
+	})).then(function() {
+		var eltsMap = {};
+		names.forEach(function(name) {
+			var elt = Object.assign({}, elts[name]); // drop proxy
+			rewriteElementPaths(name, elt, id, allDirs);
+			eltsMap[name] = elt;
+		});
+		var Block = All.api.Block.extendSchema(id, eltsMap);
+		if (id) {
+			pkg.Block = Block;
+			pkg.eltsMap = eltsMap;
+		} else {
+			All.api.Block = Block;
+		}
+	}).catch(function(err) {
+		console.error(err);
+		throw err;
+	});
+};
+
+exports.validate = function(site, pkg) {
+	return Promise.resolve().then(function() {
+		var eltsMap = pkg.eltsMap;
+		var env = site.data.env;
+		var list = Object.keys(eltsMap).map(function(key) {
+			var el = eltsMap[key];
+			if (!el.name) el.name = key;
+			return el;
+		}).sort(function(a, b) {
+			return (a.priority || 0) - (b.priority || 0);
+		});
+		eltsMap.page = Object.assign({}, eltsMap.page);
+
+		var scripts = filter(list, 'scripts');
+		var styles = filter(list, 'stylesheets');
+
+		if (env == "dev" || !pkg.dir || !site.href) {
+			eltsMap.page.scripts = scripts;
+			eltsMap.page.stylesheets = styles;
+			return Promise.resolve();
+		}
+
+		return Promise.all([
+			All.statics.bundle(site, scripts, `scripts.js`),
+			All.statics.bundle(site, styles, `styles.css`)
+		]).then(function(both) {
+			eltsMap.page.scripts = both[0];
+			eltsMap.page.stylesheets = both[1];
+		});
+	}).then(function() {
+		site.constructor = pkg.Block;
+		site.$source = toSource(pkg.eltsMap);
+		delete pkg.eltsMap;
+		delete pkg.Block;
+	});
+};
+
+function filter(elements, prop) {
+	var map = {};
+	var res = [];
+	elements.forEach(function(el) {
+		var list = el[prop];
+		if (!list) return;
+		delete el[prop];
+		if (typeof list == "string") list = [list];
+		var url, prev;
+		for (var i=0; i < list.length; i++) {
+			url = list[i];
+			prev = map[url];
+			if (prev) {
+				if (el.priority != null) {
+					if (prev.priority == null) {
+						// move prev url on top of res
+						res = res.filter(function(lurl) {
+							return lurl != url;
+						});
+					} else if (prev.priority != el.priority) {
+						console.warn(prop, url, "declared in element", el.name, "with priority", el.priority, "is already declared in element", prev.name, "with priority", prev.priority);
+						continue;
+					} else {
+						continue;
+					}
+				} else {
+					continue;
+				}
+			}
+			map[url] = el;
+			res.push(url);
+		}
+	});
+	return res;
+}
+
+function getMountPath(eltPath, id, directories) {
+	var mount = directories.find(function(mount) {
+		return eltPath.startsWith(mount.from);
+	});
+	if (!mount) {
+		console.warn(`Warning: element ${eltPath} cannot be mounted`);
+		return;
+	}
+	var basePath = id ? mount.to.replace(id + "/", "") : mount.to;
+	var eltPathname = Path.join(basePath, eltPath.substring(mount.from.length));
+	return Path.dirname(eltPathname);
+}
+
+function rewriteElementPaths(name, elt, id, directories) {
+	['scripts', 'stylesheets', 'resources'].forEach(function(what) {
+		if (elt[what] != null) {
+			elt[what] = elt[what]
+			.map(function(obj) {
+				var dir = getMountPath(obj.base, id, directories);
+				if (!dir) {
+					console.error("Could not find mount path for", obj.base, id);
+					return;
+				}
+				if (obj.path.startsWith('/') || /^(http|https|data):/.test(obj.path)) return obj.path;
+				return Path.join(dir, obj.path);
+			})
+			.filter(x => !!x);
+		} else {
+			delete elt[what];
+		}
+	});
+}
+
+function importElements(path, context, elts, names) {
+	return fs.readFile(path).then(function(buf) {
+		context.path = path;
+		var script = new vm.Script(buf, {
+			filename: path
+		});
+		var sandbox = {
+			Pageboard: {
+				elements: elts
+			}
+		};
+		script.runInNewContext(sandbox, {
+			filename: path,
+			timeout: 1000
+		});
+
+		var elt;
+		for (var name in elts) {
+			elt = elts[name];
+			if (!elt) {
+				console.warn("element", name, "is not defined at", path);
+				continue;
+			}
+			['scripts', 'stylesheets', 'resources'].forEach(function(what) {
+				var list = elt[what];
+				if (!list) return;
+				if (typeof list == "string") list = [list];
+				elt[what] = list.map(function(str) {
+					return {base: path, path: str};
+				});
+			});
+			names.push(name);
+			Object.defineProperty(elts, name, {
+				value: new Proxy(elt, new EltProxy(context, name)),
+				writable: false,
+				enumerable: false,
+				configurable: false
+			});
+		}
+	});
+}
+
+class EltProxy {
+	constructor(context, name) {
+		this.name = name;
+		this.context = context;
+	}
+	set(elt, key, val) {
+		if (this.name == "user") return false; // changing user is forbidden
+		if (key == "scripts" || key == "stylesheets" || key == "resources") {
+			val = val.map(function(str) {
+				return {
+					path: str,
+					base: this.context.path // this value change with path by design
+				};
+			}, this);
+		}
+		return Reflect.set(elt, key, val);
+	}
+}
