1602416d98e07babefb01721de5f557879cb5942
Jérémy Lal <kapouer@melix.org>
2018-04-30 10:29:48 +0200

    Move user session to settings

--- packages/api/elements/user.js
+++ packages/api/elements/user.js
@@ -5,22 +5,6 @@ Pageboard.elements.user = {
 			type: 'string',
 			format: 'email'
 		},
-		session: {
-			type: 'object',
-			properties: {
-				done: {
-					type: 'boolean',
-					default: false
-				},
-				hash: {
-					type: ['string', 'null']
-				},
-				referer: {
-					type: 'string',
-					format: 'uri'
-				}
-			}
-		},
 		origin: {
 			type: ['string', 'null'] // TODO the form id? type? that created this user
 			// could be registration, contact, newsletter forms...
@@ -39,7 +23,7 @@ Pageboard.elements.user = {
 };
 
 Pageboard.elements.settings = {
-	properties : {
+	properties: {
 		grants: {
 			type: 'array',
 			uniqueItems: true,
@@ -52,7 +36,26 @@ Pageboard.elements.settings = {
 					title: 'Webmaster'
 				}]
 			}
+		},
+		session: {
+			type: 'object',
+			properties: {
+				grants: {
+					// filled below
+				},
+				verified: {
+					type: 'boolean',
+					default: false
+				},
+				hash: {
+					type: 'string'
+				},
+				referer: {
+					type: 'string',
+					format: 'uri'
+				}
+			}
 		}
 	}
 };
-
+Pageboard.elements.settings.properties.session.properties.grants = Pageboard.elements.settings.properties.grants;
