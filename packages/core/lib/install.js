22d67275983ea8f428e9307a96f5c86436aa8529
Jérémy Lal <kapouer@melix.org>
2018-06-19 14:07:24 +0200

    dash to master

--- packages/core/lib/install.js
+++ packages/core/lib/install.js
@@ -120,7 +120,7 @@ function decideInstall(dataDir, site) {
 	var version = site.data.version;
 	if (version == "") version = site.data.version = null; // temporary fix, should not happen
 	if (version == null) {
-		version = "-";
+		version = "master";
 	} else if (/\s/.test(version) == true || semverRegex().test(version) == false && /^\w+$/.test(version) == false) {
 		return Promise.reject(new Error(`${site.id} has invalid version ${version}`));
 	}
@@ -226,7 +226,7 @@ function doInstall(site, pkg, opt) {
 }
 
 function populatePkg(site, pkg) {
-	var version = pkg.version != null ? pkg.version : site.data.version != null ? site.data.version : '-';
+	var version = pkg.version != null ? pkg.version : site.data.version != null ? site.data.version : 'master';
 	var pair = `${site.id}/${version}`;
 	var siteModuleDir = Path.join(pkg.dir, 'node_modules', pkg.name);
 	return fs.readFile(Path.join(siteModuleDir, "package.json")).then(function(buf) {
