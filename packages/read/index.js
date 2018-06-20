b0c05cc15ae7f4eec466ab2f670d2262a8f121a6
Jérémy Lal <kapouer@melix.org>
2018-06-20 02:16:17 +0200

    Most of it moves to pageboard/elements. Configurability through site.

--- packages/read/index.js
+++ packages/read/index.js
@@ -1,6 +1,3 @@
-var readFile = require('util').promisify(require('fs').readFile);
-var Path = require('path');
-
 module.exports = function(opt) {
 	return {
 		priority: 0,
@@ -12,14 +9,24 @@ function init(All) {
 	// TODO use opt.prerender to configure dom plugins
 	// TODO expose route for preload and route for load,
 	// the route for load will use the preload route as source (view helper can pipe http requests)
-
-	return readFile(Path.join(__dirname, 'read/read.html')).then(function(buf) {
-		All.app.get(
-			'*',
-			All.auth.restrict('*'),
-			All.cache.tag('api', 'share', 'file'),
-			All.dom(buf).load()
-		);
-	});
+	All.app.get(
+		'*',
+		All.auth.restrict('*'),
+		All.cache.tag('api', 'share', 'file'),
+		All.dom(function(mw, settings, req, res) {
+			var scripts = ["/.api/elements.js"].concat(req.site.$resources);
+			var tags = scripts.map(function(src) {
+				return `<script src="${src}"></script>`;
+			});
+			settings.view = `<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	${tags.join('\n')}
+</head>
+<body>
+</body>
+</html>`;
+		}).load()
+	);
 }
-
