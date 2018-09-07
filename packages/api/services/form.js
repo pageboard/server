80cbf44fb4c757c1a13bbdb3ca5cab8217c80449
Jérémy Lal <kapouer@melix.org>
2018-09-07 02:06:49 +0200

    Cleanup and simplify query/form

--- packages/api/services/form.js
+++ packages/api/services/form.js
@@ -1,4 +1,4 @@
-var URL = require('url');
+const URL = require('url');
 
 exports = module.exports = function(opt) {
 	return {
@@ -9,114 +9,51 @@ exports = module.exports = function(opt) {
 
 function init(All) {
 	All.app.get("/.api/form", function(req, res, next) {
-		All.run('form.query', req.site, req.query).then(function(data) {
-			res.json(data);
-		}).catch(next);
+		next(new HttpError.MethodNotAllowed("Only post allowed"));
 	});
-	All.app.post("/.api/form", function(req, res, next) {
-		req.body._referer = req.headers.referer;
-		All.run('form.submit', req.site, req.body).then(function(data) {
-			if (data.redirect && req.accepts('html') && !req.xhr) {
-				res.location(data.redirect);
-			}	else {
-				res.json(data);
-			}
+	All.app.post("/.api/form/:id", function(req, res, next) {
+		All.run('form.submit', req.site, {
+			id: req.params.id,
+			query: All.utils.unflatten(URL.parse(req.headers.referer, true).query),
+			body: All.utils.unflatten(req.body)
+		}).then(function(data) {
+			res.json(data);
 		}).catch(next);
 	});
 }
 
-exports.query = function(site, data) {
-	return All.run('block.get', site, {
-		id: data._id
-	}).then(function(form) {
-		var fd = form.data.action || {};
-		if (!fd.type) throw new HttpError.BadRequest("Missing form action.type");
-		var id = fd.vars && fd.vars.id && data[fd.vars.id] || data.id;
-		return All.run('block.get', site, {
-			id: id,
-			type: fd.type
-		});
-	});
-};
-exports.query.schema = {
-	required: ["_id"],
-	properties: {
-		_id: {
-			type: 'string'
-		}
-	}
-};
-
 exports.submit = function(site, data) {
-	var referer = URL.parse(data._referer, true);
-	delete data._referer;
 	return All.run('block.get', site, {
-		id: data._id
+		id: data.id
 	}).then(function(form) {
-		var fd = form.data.action || {};
-		if (fd.method != "post") throw new HttpError.MethodNotAllowed("Only post allowed");
-		delete data._id;
-
-		var setVar = All.search.setVar;
-		var getVar = All.search.getVar;
-		var params = {}; // TODO import data using fd.type schema
-		Object.keys(data).forEach(function(key) {
-			setVar(params, key, data[key]);
-		});
-		if (fd.vars) Object.keys(fd.vars).forEach(function(key) {
-			var val = getVar(data, fd.vars[key]);
-			if (val === undefined) return;
-			setVar(params, fd.vars[key]);
-			setVar(params, key, val);
+		var fd = form.data || {};
+		if (!fd.method) throw new HttpError.BadRequest("Missing method");
+		var params = mergeParameters(fd.parameters, {
+			$query: data.query,
+			$body: data.body
 		});
 		if (fd.type) {
-			// when bound to an element, all keys are supposed to be in block.data
-			var id = params.id;
-			delete params.id;
-			var parent = params.parent;
-			delete params.parent;
-			data = params;
-			params = {
-				id: id,
-				type: fd.type
-			};
-			if (Object.keys(data).length > 0) {
-				params.data = data;
-			}
-			if (parent) params.parent = parent;
+			if (Object.keys(data.body).length > 0) params.data = data.body;
+			params.type = fd.type;
 		}
-		// overwriting values
-		if (fd.consts) Object.keys(fd.consts).forEach(function(key) {
-			setVar(params, key, fd.consts[key]);
-		});
-		return All.run(fd.call, site, params).then(function(response) {
-			if (typeof response != "obj") response = {};
-			var fd = form.data.redirection;
-			if (fd.url) {
-				var query = {};
-				var obj = URL.parse(fd.url);
-				delete obj.path;
-				obj.query = query;
-				if (fd.vars) Object.keys(fd.vars).forEach(function(key) {
-					var val = getVar(referer.query, fd.vars[key]);
-					if (val === undefined) return;
-					setVar(query, key, val);
-				});
-				if (fd.consts) Object.keys(fd.consts).forEach(function(key) {
-					setVar(query, key, fd.consts[key]);
-				});
-				response.redirect = URL.format(obj);
-			}
-			return response;
-		});
+		return All.run(fd.method, site, params);
 	});
 };
+
 exports.submit.schema = {
-	required: ["_id"],
+	$action: 'write',
+	required: ["id"],
 	properties: {
-		_id: {
+		id: {
 			type: 'string'
+		},
+		query: {
+			type: 'object'
+		},
+		body: {
+			type: 'object'
 		}
-	}
+	},
+	additionalProperties: false
 };
 
--- packages/api/services/query.js
+++ packages/api/services/query.js
@@ -1,4 +1,4 @@
-var lodashMerge = require('lodash.merge');
+const URL = require('url');
 
 exports = module.exports = function(opt) {
 	return {
@@ -7,18 +7,12 @@ exports = module.exports = function(opt) {
 	};
 };
 
-/*
-- form.get
-- builds a url query
-- opens a page
-- the page contains a query block
-- the query block requests the query api to return blocks
-- the query block renders the blocks into its virtual content
-*/
-
 function init(All) {
-	All.app.get("/.api/query", function(req, res, next) {
-		All.run('search.query', req.site, req.query).then(function(data) {
+	All.app.get("/.api/query/:id", function(req, res, next) {
+		All.run('search.query', req.site, {
+			id: req.params.id,
+			query: All.utils.unflatten(URL.parse(req.headers.referer, true).query)
+		}).then(function(data) {
 			res.json(data);
 		}).catch(next);
 	});
@@ -29,64 +23,28 @@ function init(All) {
 
 exports.query = function(site, data) {
 	return All.run('block.get', site, {
-		id: data._id
+		id: data.id
 	}).then(function(parent) {
-		var fd = parent.data.request || parent.data || {};
-		var apiCall = fd.method || fd.call;
-		if (!apiCall) throw new HttpError.BadRequest("Missing data.api");
-		delete data._id;
-		var params = {};
-		// consts: destPath: val
-		// vars: destPath: queryPath
-		// allow rewriting variables
-		var copy = lodashMerge({}, data);
-		if (fd.vars) Object.keys(fd.vars).forEach(function(key) {
-			var varName = fd.vars[key];
-			if (!varName) return;
-			varName = varName.split('|').shift();
-			var val = getVar(data, varName);
-			if (val === undefined) return;
-			setVar(copy, varName); // problem, it removes the variables but might need it later...
-			setVar(params, key, val);
+		var fd = parent.data || {};
+		if (!fd.method) throw new HttpError.BadRequest("Missing method");
+		var params = All.utils.mergeParameters(fd.parameters, {
+			$query: data.query
 		});
-		if (fd.type) {
-			params.type = fd.type;
-			if (Object.keys(copy).length > 0) {
-				params.data = copy;
-			}
-		}
-		// overwriting values
-		if (fd.consts) Object.keys(fd.consts).forEach(function(key) {
-			setVar(params, key, fd.consts[key]);
-		});
-
-		return All.run(apiCall, site, params);
+		return All.run(fd.method, site, params);
 	});
 };
 
-exports.getVar = getVar;
-function getVar(obj, path) {
-	var list = path.split('.');
-	var name;
-	for (var i=0; i < list.length; i++) {
-		name = list[i];
-		if (obj[name] == null) return;
-		obj = obj[name];
-	}
-	return obj;
-}
-
-exports.setVar = setVar;
-function setVar(obj, path, val) {
-	var list = path.split('.');
-	var last = list.pop();
-	var name;
-	for (var i=0; i < list.length; i++) {
-		name = list[i];
-		if (obj[name] == null) obj[name] = {};
-		obj = obj[name];
-	}
-	if (val === undefined) delete obj[last];
-	else obj[last] = val;
-}
+exports.query.schema = {
+	$action: 'read',
+	required: ['id'],
+	properties: {
+		id: {
+			type: 'string'
+		},
+		query: {
+			type: 'object'
+		}
+	},
+	additionalProperties: false
+};
 
