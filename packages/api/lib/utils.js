68f0f78608d28d1b9e11fe78c4feef1ce275601e
Jérémy Lal <kapouer@melix.org>
2018-09-25 12:17:56 +0200

    matchdom eval

--- packages/api/lib/utils.js
+++ packages/api/lib/utils.js
@@ -15,16 +15,14 @@ function mergeParameters(params, obj) {
 	if (params) Object.keys(params).forEach(function(key) {
 		var val = params[key];
 		if (typeof val == "string") {
-			var str = `[${val}|valcb]`;
-			var nstr = matchdom(str, obj, {
-				valcb: function(val, what) {
-					var path = what.scope.path.slice();
-					var last = path.pop();
-					var parent = what.expr.get(what.data, path);
-					delete parent[last];
-					return val;
-				}
-			});
+			var str = `[${val}]`;
+			var nstr = matchdom(str, obj, {'||': function(val, what) {
+				var path = what.scope.path.slice();
+				var last = path.pop();
+				var parent = what.expr.get(what.data, path);
+				delete parent[last];
+				return val;
+			}});
 			if (nstr != str) params[key] = nstr;
 		} else if (typeof val == "object") {
 			mergeParameters(val, obj);
