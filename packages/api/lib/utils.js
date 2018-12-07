ff0df869eaec818d75ef77265f9539525769bb56
Jérémy Lal <kapouer@melix.org>
2018-12-07 09:36:32 +0100

    api: mergeParameters supports $response as well

--- packages/api/lib/utils.js
+++ packages/api/lib/utils.js
@@ -18,7 +18,7 @@ function mergeParameters(params, obj, ret) {
 		if (typeof val == "string") {
 			matchdom(`[${val}]`, obj, {'||': function(val, what) {
 				var path = what.scope.path.slice();
-				if (path[0] == "$query" || path[0] == "$body") {
+				if (path[0] == "$query" || path[0] == "$body" || path[0] == "$response") {
 					var last = path.pop();
 					var parent = what.expr.get(what.data, path);
 					delete parent[last];
