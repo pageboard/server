326fb9e3c5f6cdf03cc6cc8480544e51eb5b093c
Jérémy Lal <kapouer@melix.org>
2018-12-13 09:19:54 +0100

    api: mergeParameters now allow expressions within strings

--- packages/api/lib/utils.js
+++ packages/api/lib/utils.js
@@ -12,17 +12,21 @@ exports.unflatten = function(query) {
 exports.mergeParameters = mergeParameters;
 
 function mergeParameters(params, obj, ret) {
+	// consumed obj parameters are removed from obj
+	// this is useful for populating a block with matchdom expressions
+	// that merge variables but remove them as they are merged.
+	// it is avoidable, especially with the new template variables,
+	// the block data could be properly scoped and not mixed with request parameters.
 	if (!ret) ret = {};
 	Object.keys(params).forEach(function(key) {
 		var val = ret[key] = params[key];
 		if (typeof val == "string") {
-			matchdom(`[${val}]`, obj, {'||': function(val, what) {
+			ret[key] = matchdom(val, obj, {'||': function(val, what) {
 				var path = what.scope.path.slice();
 				if (path[0] == "$query" || path[0] == "$body" || path[0] == "$response") {
 					var last = path.pop();
 					var parent = what.expr.get(what.data, path);
 					delete parent[last];
-					ret[key] = val;
 				}
 				return val;
 			}});
