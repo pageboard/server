fc49954fefa43a3c786df22f4dff1e3b6ff75798
Jérémy Lal <kapouer@melix.org>
2018-12-24 10:30:38 +0100

    api: avoid crash in mergeParameters

--- packages/api/lib/utils.js
+++ packages/api/lib/utils.js
@@ -26,6 +26,7 @@ function mergeParameters(params, obj, ret) {
 				if (path[0] == "$query" || path[0] == "$body" || path[0] == "$response") {
 					var last = path.pop();
 					var parent = what.expr.get(what.data, path);
+					if (parent == null) return;
 					delete parent[last];
 				}
 				return val;
