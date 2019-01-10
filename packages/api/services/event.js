572f108f476c75737ddd47e5743ac4986ec113a2
Jérémy Lal <kapouer@melix.org>
2019-01-10 13:37:35 +0100

    api event subscribe: schemas, parents, fixes

--- packages/api/services/event.js
+++ packages/api/services/event.js
@@ -1,111 +1,89 @@
-var ref = require('objection').ref;
-
 exports = module.exports = function(opt) {
 	return {
-		name: 'event'
+		name: 'event',
+		service: function() {} // FIXME remove me ?
 	};
 };
 
 exports.subscribe = function(site, data) {
-	return All.run('settings.save', site, {
-		email: data.email,
-		data: {
-			event: data.settings
+	var [pSettings, pDate] = data.parents;
+	if (pSettings.type != "settings") throw new Error("Wrong parents, expected settings, date");
+	return All.run('block.find', site, Object.assign({}, pDate, {
+		parents: {type: 'event', first: true}
+	})).then(function(result) {
+		var eventDate = result.item;
+		var maxSeats = eventDate.data.seats || eventDate.parent.data.seats || 0;
+		var total = data.reservation.seats + (eventDate.data.reservations || 0);
+		if (maxSeats > 0 && total > maxSeats) {
+			throw new HttpError.BadRequest("Cannot reserve that much seats");
 		}
-	}).then(function(settings) {
-		return All.run('block.find', site, {
-			id: data.id,
-			type: 'event_date',
-			parents: {type: 'event', first: true}
-		}).then(function(foundObj) {
-			var eventDate = foundObj.data;
-			var total = data.reservation.seats + (eventDate.data.reservations || 0);
-			if (eventDate.data.seats > 0 && total > eventDate.data.seats) {
-				throw new HttpError.BadRequest("Cannot reserve that much seats");
+		return eventDate.$query().patch({
+			'data:reservations': total
+		}).then(function() {
+			return eventDate;
+		});
+	}).then(function(eventDate) {
+		// add event_reservation block with two parents: settings and event_date
+		return All.run('block.search', site, {
+			type: 'event_reservation',
+			parent: {
+				id: data.parents.map(x => x.id)
 			}
-			return eventDate.$query().patch({
-				'data:reservations': total
-			}).then(function() {
-				return eventDate;
-			});
-		}).then(function(eventDate) {
-			// add event_reservation block with two parents: settings and event_date
-			return All.run('block.add', site, {
-				type: 'event_reservation',
-				data: data.reservation
-			}).then(function(reservation) {
-				return reservation.$relatedQuery('parents', site.trx).relate([
-					eventDate._id,
-					settings._id
-				]).then(function() {
-					return {
-						date: eventDate,
-						reservation: reservation
-					};
-				});
-			});
 		}).then(function(obj) {
-			if (!data.url) return obj; // can't send confirmation email
-			var date = obj.date;
-			var event = date.parent;
-			var resa = obj.reservation;
-			return All.run('mail.send', site, {
-				url: data.url,
-				to: data.email,
-				query: {
-					title: event.data.title,
-					venue: event.data.venue,
-					begin: date.data.slot.start,
-					end: date.data.slot.end,
-					seats: resa.data.seats,
-					name: resa.data.name,
-					groups: event.data.groupsOnly
-				}
+			var blockMeth, resa;
+			if (obj.items.length == 1) {
+				resa = obj.items[0];
+				Object.assign(resa.data, data.reservation);
+				blockMeth = 'block.save';
+			} else if (obj.items.length == 0) {
+				blockMeth = 'block.add';
+				resa = {
+					type: 'event_reservation',
+					data: data.reservation,
+					parents: data.parents
+				};
+			} else {
+				throw new Error("Two reservations using the same login");
+			}
+			return All.run(blockMeth, site, resa).then(function(resa) {
+				if (!data.url) return resa; // can't send confirmation email
+				var event = eventDate.parent;
+				return All.run('mail.send', site, {
+					url: data.url,
+					to: pSettings.id,
+					query: {
+						title: event.data.title,
+						venue: event.data.venue,
+						begin: eventDate.data.slot.start,
+						end: eventDate.data.slot.end,
+						seats: resa.data.seats,
+						name: resa.data.name,
+						groups: event.data.groupsOnly
+					}
+				});
 			});
 		});
 	});
 };
 
 exports.subscribe.schema = {
+	title: 'Subscribe',
 	$action: 'write',
-	required: ['id', 'email', 'settings', 'reservation'],
+	required: ['parents', 'reservation'],
 	properties: {
-		id: {
-			type: 'string',
-			format: 'id'
-		},
-		email: {
-			type: 'string',
-			format: 'email'
-		},
-		url: {
-			title: 'email page template',
-			nullable: true,
-			anyOf: [{
-				type: "string",
-				format: "uri"
-			}, {
-				type: "string",
-				format: "pathname"
-			}],
-		},
-		settings: {
-			type: 'object',
-			default: {},
-			properties: {
-				allowNews: {
-					title: 'Allow sending news',
-					type: 'boolean',
-					default: false
-				},
-				allowEmail: {
-					title: 'Allow emails',
-					type: 'boolean',
-					default: false
-				}
+		parents: {
+			title: 'parents',
+			type: 'array',
+			items: {
+				type: 'object'
+			},
+			$filter: {
+				name: 'relation',
+				from: 'service'
 			}
 		},
 		reservation: {
+			title: 'Reservation',
 			type: 'object',
 			required: ['seats', 'name'],
 			properties: {
@@ -129,7 +107,48 @@ exports.subscribe.schema = {
 					pattern: '^\\d+(\\s*\\.*-*\\d+)*$'
 				}
 			}
+		},
+		url: {
+			title: 'Mail page',
+			type: "string",
+			format: "uri-reference",
+			nullable: true,
+			$helper: {
+				name: 'page',
+				title: 'Query',
+				description: 'Values can be [$query.xxx]',
+				query: true,
+				type: 'mail'
+			}
 		}
+	},
+	parents: {
+		type: 'array',
+		items: [{
+			type: 'object',
+			properties: {
+				type: {
+					const: 'settings'
+				},
+				id: {
+					title: 'user settings id',
+					type: 'string',
+					format: 'id'
+				}
+			}
+		}, {
+			type: 'object',
+			properties: {
+				type: {
+					const: 'event_date'
+				},
+				id: {
+					title: 'event date id',
+					type: 'string',
+					format: 'id'
+				}
+			}
+		}]
 	}
 };
 exports.subscribe.external = true;
@@ -138,7 +157,17 @@ exports.unsubscribe = function(site, data) {
 
 };
 exports.unsubscribe.schema = {
+	title: 'Unsubscribe',
 	$action: 'write',
-	type:'object'
+	type: 'object',
+	parents: exports.subscribe.parents,
+	required: ['parents'],
+	properties: {
+		parents: {
+			title: 'parents',
+			type: 'array',
+			$filter: 'relation'
+		}
+	}
 };
 exports.unsubscribe.external = true;
