a678c68621ae4952ad1f173a6ea549df020510bf
Jérémy Lal <kapouer@melix.org>
2019-04-17 11:35:17 +0200

    event.subscribe: restore lock

--- packages/api/services/event.js
+++ packages/api/services/event.js
@@ -43,7 +43,7 @@ exports.subscribe = function(req, data) {
 					type: 'event_reservation',
 					data: data.reservation,
 					parents: data.parents,
-					lock: [`id-${req.user.id}`, 'scheduler']
+					lock: {read: [`id-${req.user.id}`, 'scheduler']}
 				};
 			} else {
 				throw new Error("Two reservations using the same login");
