b9b36ccd33d2ab53878cd56c0f7993eb4f339377
Jérémy Lal <kapouer@melix.org>
2019-04-18 15:32:52 +0200

    Backport github processing

--- packages/github-webhook/index.js
+++ packages/github-webhook/index.js
@@ -1,4 +1,5 @@
-var crypto = require('crypto');
+const semver = require('semver');
+const crypto = require('crypto');
 
 exports = module.exports = function(opt) {
 	return {
@@ -11,13 +12,13 @@ function init(All) {
 	console.info("Setting up /.api/github webhook");
 	All.app.post('/.api/github', function(req, res, next) {
 		var site = req.site;
-		var save = false;
+		var version;
 		Promise.resolve().then(function() {
 			var event = req.get('X-Github-Event');
 			if (event == "ping") {
 				return res.sendStatus(200);
 			}
-			if (event != "push" && event != "create") {
+			if (event != "push") {
 				return next(new HttpError.BadRequest("Unsupported event"));
 			}
 
@@ -25,38 +26,99 @@ function init(All) {
 			if (secret) {
 				var sign = req.get('X-Github-Signature');
 				if (sign && sign != signBlob(secret, req._body)) {
-					throw new HttpError.Forbidden("Invalid Signature");
+					return next(new HttpError.Forbidden("Invalid Signature"));
 				}
 			}
 
 			var payload = req.body;
-			var module = site.data.module;
-			var version;
-			if (module && module == payload.repository.full_name) {
-				var ref = payload.ref;
-				if (payload.ref_type == "tag" && ref) {
-					version = ref;
-				} else if (site.data.env != "production") {
-					version = payload.after;
-				}
-				if (version) {
-					site.data.version = version;
-					save = true;
-				} else {
-					console.warn("Nothing to deploy", site.data.env, event, ref);
-				}
-			} else {
-				throw new HttpError.BadRequest(`Unknown module "${payload.repository.full_name}"`);
+			pusher = payload.pusher;
+			const mod = parseRefs(site.data.module);
+
+			if (!mod.repo || mod.repo != payload.repository.full_name) {
+				return next(new HttpError.BadRequest(`Unknown repository "${payload.repository.full_name}"`));
 			}
-			res.status(200);
-			if (save) res.send(`Saving version ${version}`);
-			else res.send('Nothing to do');
-		}).catch(next).then(function() {
-			if (save) return All.site.save(site).catch(function(err) {
-				console.error("Could not deploy site", site.data.version, err);
+			var refs = getRefs(payload);
+			if (!refs) {
+				return next(new HttpError.BadRequest('Ignoring payload without ref'));
+			}
+
+			// site.data.module keeps the repository, branch|commit|tag|semver:range that should be installed
+			// site.data.version tracks the *actual* successfully installed commit|tag|version
+			var msg = 'Nothing to do';
+			var stop = false;
+			if (mod.branch && refs.branch) {
+				if (refs.branch != mod.branch) {
+					msg = `Site module restricted to branch "${mod.branch}"`;
+					stop = true;
+				}
+			}
+			if (!stop && refs.version) {
+				if (mod.range && semver.satisfies(refs.version, mod.range) == false) {
+					msg = `Site module restricted to version "${mod.range}"`;
+					stop = true;
+				} else if (!version) {
+					version = refs.version;
+				}
+			}
+			if (!stop && refs.tag) {
+				if (mod.tag && refs.tag != mod.tag) {
+					msg = `Site module restricted to tag "${mod.tag}"`;
+					stop = true;
+				} else if (!version) {
+					version = refs.tag;
+				}
+			}
+			if (!stop && refs.commit) {
+				if (mod.commit && refs.commit != mod.commit) {
+					msg = `Site module restricted to commit "${mod.commit}"`;
+					stop = true;
+				} else if (!version && site.data.env != "production") {
+					version = refs.commit;
+				}
+			}
+			if (version != null && !stop) {
+				site.data.version = version;
+				msg = `Saving version ${version}`;
+			}
+			res.status(200).send(msg);
+		}).catch(function(err) {
+			next(err);
+			throw err;
+		}).then(function() {
+			if (version != null) return All.site.save(site).catch(function(err) {
+				console.error(site.id, site.data.module, err);
 			});
 		});
 	});
+}
+
+function getRefs(pay) {
+	if (!pay.ref && !pay.after || pay.deleted) return;
+	const obj = {
+		tag: (/refs\/tags\/(.+)/.exec(pay.ref) || []).pop(),
+		branch: (/refs\/heads\/(.+)/.exec(pay.base_ref || pay.ref) || []).pop(),
+		commit: pay.after
+	};
+	if (obj.tag) obj.version = semver.clean(obj.tag);
+	return obj;
+}
+
+function parseRefs(str) {
+	const [repo, com] = (str || '').split('#');
+	const obj = {
+		repo: repo
+	};
+	if (com) {
+		if (/^#[a-z0-9]{8,}$/.test(com)) {
+			obj.commit = com;
+		} else if (com.startsWith('semver:')) {
+			obj.tag = com.substring(7);
+			obj.range = semver.validRange(obj.tag);
+		} else {
+			obj.branch = com;
+		}
+	}
+	return obj;
 }
 
 function signBlob(key, blob) {
