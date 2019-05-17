e8fd57fb2155c4febb0b357621cbdfa9b887085f
Jérémy Lal <kapouer@melix.org>
2019-05-17 09:31:54 +0200

    upload: just use 0.6 version

--- packages/upload/index.js
+++ packages/upload/index.js
@@ -1,11 +1,13 @@
-var multer = require('multer');
-var Path = require('path');
-var crypto = require('crypto');
-var mkdirp = require('mkdirp');
-var pify = require('util').promisify;
-var mkdirpp = pify(mkdirp);
-var speaking = require('speakingurl');
-var fs = {
+const multer = require('multer');
+const Path = require('path');
+const crypto = require('crypto');
+const mkdirp = require('mkdirp');
+const typeis = require('type-is');
+const mime = require('mime-types');
+const pify = require('util').promisify;
+const mkdirpp = pify(mkdirp);
+const speaking = require('speakingurl');
+const fs = {
 	unlink: pify(require('fs').unlink)
 };
 
@@ -21,70 +23,122 @@ exports = module.exports = function(opt) {
 		from: dest,
 		to: opt.upload.dir
 	});
+	opt.upload.path = dest;
 
 	return {
 		name: 'upload',
-		service: init,
-		dest: dest
+		service: function(All) {
+			return mkdirpp(dest).then(function() {
+				return init(All);
+			});
+		}
 	};
 };
 
 function init(All) {
 	var upload = All.opt.upload;
-	var dest = this.dest;
-	return mkdirpp(dest).then(function() {
-		var storage = multer.diskStorage({
-			destination: function(req, file, cb) {
-				var date = (new Date()).toISOString().split('T').shift().substring(0, 7);
-				var curDest = Path.join(dest, req.site.id, date);
+	var storage = multer.diskStorage({
+		destination: function(req, file, cb) {
+			var date = (new Date()).toISOString().split('T').shift().substring(0, 7);
+			var curDest = Path.join(upload.path, req.site.id, date);
 
-				mkdirp(curDest, function(err) {
-					if (err) return cb(err);
-					cb(null, curDest);
+			mkdirp(curDest, function(err) {
+				if (err) return cb(err);
+				cb(null, curDest);
+			});
+		},
+		filename: function (req, file, cb) {
+			var parts = file.originalname.split('.');
+			var basename = speaking(parts.shift(), {truncate: 128});
+			var extensions = parts.map(function(str) {
+				return speaking(str, {
+					symbols: false,
+					truncate: 8
 				});
-			},
-			filename: function (req, file, cb) {
-				var parts = file.originalname.split('.');
-				var basename = speaking(parts.shift(), {truncate: 128});
-				var extensions = parts.map(function(str) {
-					return speaking(str, {
-						symbols: false,
-						truncate: 8
-					});
-				}).join('.').toLowerCase();
+			}).join('.').toLowerCase();
 
-				crypto.pseudoRandomBytes(4, function (err, raw) {
-					if (err) return cb(err);
-					cb(null, `${basename}-${raw.toString('hex')}.${extensions}`);
-				});
-			}
-		});
+			crypto.pseudoRandomBytes(4, function (err, raw) {
+				if (err) return cb(err);
+				cb(null, `${basename}-${raw.toString('hex')}.${extensions}`);
+			});
+		}
+	});
 
-		var mw = multer({
-			storage: storage,
-			limits: {
+	All.app.post('/.api/upload/:id?', function(req, res, next) {
+		Promise.resolve().then(function() {
+			var limits = {
 				files: upload.files,
-				fileSize: upload.size
-			}
-		});
-
-		All.app.post('/.api/upload', mw.array('files'), function(req, res, next) {
-			var curDest = Path.join(dest, req.site.id);
-			var href = req.site.href;
-			Promise.all(req.files.map(function(file) {
-				return All.image.upload(file).then(function() {
-					return href + '/.' + Path.join(
-						upload.dir,
-						Path.relative(curDest, file.destination),
-						file.filename
-					);
+				size: upload.size,
+				types: ['*/*']
+			};
+			if (req.params.id) {
+				return All.run('block.get', req, {id: req.params.id}).then(function(input) {
+					return Object.assign(limits, input.data.limits);
 				});
-			})).then(function(list) {
-				res.send(list);
-			}).catch(next);
+			} else {
+				return limits;
+			}
+		}).then(function(limits) {
+			multer({
+				storage: storage,
+				fileFilter: function(req, file, cb) {
+					var types = limits.types.length ? limits.types : ['*/*'];
+					cb(null, !!typeis.is(file.mimetype, types));
+				},
+				limits: {
+					files: limits.files,
+					fileSize: limits.size
+				}
+			}).array('files')(req, res, function() {
+				return Promise.all(req.files.map(function(file) {
+					return exports.file(req, file);
+				})).then(function(list) {
+					// backward compatibility with elements-write's input href
+					var obj = req.params.id ? {items: list} : list;
+					res.send(obj);
+				}).catch(next);
+			});
 		});
 	});
 }
+
+exports.file = function({site}, data) {
+	var upload = All.opt.upload;
+	var dest = Path.join(upload.path, site.id);
+	if (!data.filename) data.filename = Path.basename(data.path);
+	if (!data.destination) data.destination = Path.dirname(data.path);
+	if (!data.mimetype) data.mimetype = mime.lookup(Path.extname(data.filename));
+
+	return All.image.upload(data).then(function() {
+		return '/.' + Path.join(
+			upload.dir,
+			Path.relative(dest, data.destination),
+			data.filename
+		);
+	});
+};
+exports.file.schema = {
+	title: 'Upload file',
+	required: ['path'],
+	properties: {
+		path: {
+			title: 'File path',
+			type: 'string'
+		},
+		filename: {
+			type: 'string',
+			nullable: true
+		},
+		destination: {
+			type: 'string',
+			nullable: true
+		},
+		mimetype: {
+			type: 'string',
+			nullable: true
+		}
+	}
+};
 
 exports.gc = function(id, pathname) {
 	var uploadDir = All.opt.upload.dir;
