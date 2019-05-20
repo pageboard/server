e2f13299dd7b46621c5ac55dde87290f3542d480
Jérémy Lal <kapouer@melix.org>
2019-05-21 01:20:50 +0200

    pdf: a4, 10mm margins

--- packages/pdf/lib/pdf.js
+++ packages/pdf/lib/pdf.js
@@ -2,6 +2,14 @@ var dom = require('express-dom');
 
 exports.helper = function(mw, settings, request, response) {
 	if (request.path.endsWith('.pdf') == false) return;
+	settings.pdf = {
+		defaults: {
+			paper: 'iso_a4',
+			margins: '10mm'
+		},
+		mappings: {
+		}
+	};
 	settings.load.plugins = [
 		dom.plugins.upcache,
 		dom.plugins.bearer,
