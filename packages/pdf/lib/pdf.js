764702fa637dc4460cd6b265726830959195f304
Jérémy Lal <kapouer@melix.org>
2019-05-20 15:27:01 +0200

    pdf module

---
+++ packages/pdf/lib/pdf.js
@@ -0,0 +1,14 @@
+var dom = require('express-dom');
+
+exports.helper = function(mw, settings, request, response) {
+	if (request.path.endsWith('.pdf') == false) return;
+	settings.load.plugins = [
+		dom.plugins.upcache,
+		dom.plugins.bearer,
+		dom.plugins.prerender,
+		dom.plugins.pdf
+	];
+};
+
+exports.plugin = require('express-dom-pdf').plugin;
+
