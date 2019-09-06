3e6d636ea87ed15e22a07cb126cf203fe1baab9a
Jérémy Lal <kapouer@melix.org>
2019-09-06 12:42:28 +0200

    prerender: pass state to serialize

--- packages/prerender/plugins/serialize.js
+++ packages/prerender/plugins/serialize.js
@@ -1,9 +1,9 @@
 module.exports = function(page, settings, request, response) {
 	page.when('idle', function() {
 		if (settings.output == null) return page.run(function(done) {
-			Page.finish(function() {
+			Page.finish(function(state) {
 				Promise.resolve().then(function() {
-					if (Page.serialize) return Page.serialize();
+					if (Page.serialize) return Page.serialize(state);
 					else return '<!DOCTYPE html>\n' + document.documentElement.outerHTML;
 				}).then(function(doc) {
 					done(null, doc);
