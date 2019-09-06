401f503304992b2a6b80a04d859a1e1d3340e30c
Jérémy Lal <kapouer@melix.org>
2019-09-06 10:47:04 +0200

    api: query and form must return < 500 status codes

--- packages/api/services/query.js
+++ packages/api/services/query.js
@@ -40,7 +40,7 @@ exports.query = function(req, data) {
 		params = All.utils.mergeObjects(params, fd.action.parameters);
 		return All.run(method, req, params).catch(function(err) {
 			return {
-				status: err.status || 500,
+				status: err.status || 400,
 				item: {
 					type: 'error',
 					data: {
