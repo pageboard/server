0ae1607d64a39d1a47e68d721c7b834170e9e9df
Jérémy Lal <kapouer@melix.org>
2019-09-13 00:58:10 +0200

    api: listPages need to be fixed

--- packages/api/services/page.js
+++ packages/api/services/page.js
@@ -188,7 +188,7 @@ exports.get = function(req, data) {
 
 		return Promise.all([
 			getParents(site, data.url),
-			listPages(site, {
+			listPages(req, {
 				parent: data.url.split('/').slice(0, -1).join('/') || '/'
 			}).clearSelect().select([
 				ref('block.data:url').as('url'),
@@ -246,7 +246,7 @@ function getParents(site, url) {
 	.orderByRaw("length(block.data->>'url') DESC");
 }
 
-function listPages(site, data) {
+function listPages({site, trx}, data) {
 	var q = site.$relatedQuery('children')
 	.select()
 	.omit(['content'])
@@ -396,8 +396,9 @@ exports.search.schema = {
 };
 exports.search.external = true;
 
-exports.all = function({site}, data) {
-	return listPages(site, data).then(function(pages) {
+exports.all = function(req, data) {
+	var site = req.site;
+	return listPages(req, data).then(function(pages) {
 		var els = {};
 		var obj = {
 			items: pages
