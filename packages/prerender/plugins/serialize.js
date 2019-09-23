ffa90379fa32a278731c7948167809dcefcef1e3
Jérémy Lal <kapouer@melix.org>
2019-09-23 21:41:29 +0200

    prerender: serialize cannot rely on Page.finish

--- packages/prerender/plugins/serialize.js
+++ packages/prerender/plugins/serialize.js
@@ -1,13 +1,15 @@
 module.exports = function(page, settings, request, response) {
 	page.when('idle', function() {
 		if (settings.output == null) return page.run(function(done) {
-			Page.finish(function(state) {
-				Promise.resolve().then(function() {
-					if (Page.serialize) return Page.serialize(state);
-					else return '<!DOCTYPE html>\n' + document.documentElement.outerHTML;
-				}).then(function(doc) {
-					done(null, doc);
-				}).catch(done);
+			Page.patch(function(state) {
+				state.finish(function() {
+					Promise.resolve().then(function() {
+						if (Page.serialize) return Page.serialize(state);
+						else return '<!DOCTYPE html>\n' + document.documentElement.outerHTML;
+					}).then(function(doc) {
+						done(null, doc);
+					}).catch(done);
+				});
 			});
 		}).then(function(obj) {
 			if (!obj) throw new HttpError.BadRequest("Empty response");
