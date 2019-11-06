139c73322154e7f5073e09f3a1f89167cd6eb680
Jérémy Lal <kapouer@melix.org>
2019-11-06 10:59:10 +0100

    db: add href to tsv vector

--- packages/api/migrations/03-href-tsv.js
+++ packages/api/migrations/03-href-tsv.js
@@ -2,11 +2,17 @@
 exports.up = function(knex) {
 	return knex.schema
 	.raw("ALTER TABLE href ADD COLUMN tsv tsvector")
+	.raw(`CREATE OR REPLACE FUNCTION href_tsv_url(url TEXT) RETURNS TEXT AS $$
+BEGIN
+	RETURN replace(regexp_replace(coalesce(url, ''), '^https?://', ''), '/', ' ');
+END
+$$ LANGUAGE plpgsql`)
 	.raw(`CREATE OR REPLACE FUNCTION href_tsv_update() RETURNS trigger AS $$
 BEGIN
 	new.tsv := setweight(to_tsvector('unaccent', coalesce(new.title, '')), 'A') ||
-		setweight(to_tsvector('unaccent', coalesce(new.site, '')), 'B') ||
-		setweight(to_tsvector('unaccent', coalesce(new.meta->>'description', '')), 'C')
+		setweight(to_tsvector('unaccent', href_tsv_url(new.url)), 'B') ||
+		setweight(to_tsvector('unaccent', coalesce(new.site, '')), 'C') ||
+		setweight(to_tsvector('unaccent', coalesce(new.meta->>'description', '')), 'D')
 		;
 	RETURN new;
 END
