58b041c98ebe01a3001801b7adc28832a9d2e5c5
Jérémy Lal <kapouer@melix.org>
2019-11-15 11:41:25 +0100

    core: default csp allow data:

--- packages/core/index.js
+++ packages/core/index.js
@@ -295,7 +295,7 @@ function createApp(All) {
 	app.use(All.domains.init);
 	app.use(function(req, res, next) {
 		res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-		res.setHeader('Content-Security-Policy', "default-src 'self'");
+		res.setHeader('Content-Security-Policy', "default-src 'self' data:");
 		res.setHeader('X-XSS-Protection','1;mode=block');
 		res.setHeader('X-Frame-Options', 'sameorigin');
 		res.setHeader('X-Content-Type-Options', 'nosniff');
