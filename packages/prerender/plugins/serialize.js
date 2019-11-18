3955280f7ce9631874e28e1f7d4fe105ff2e0e67
Jérémy Lal <kapouer@melix.org>
2019-11-18 12:17:28 +0100

    prerender: catch queue errors

--- packages/prerender/plugins/serialize.js
+++ packages/prerender/plugins/serialize.js
@@ -1,13 +1,16 @@
 module.exports = function(page, settings, request, response) {
 	page.when('idle', function() {
 		if (settings.output == null) return page.run(function(done) {
-			Page.finish(function(state) {
-				Promise.resolve().then(function() {
+			// could use Page.finish().then() the day all clients use window-page@9.2.0
+			Page.init(function(state) {
+				state.queue.then(function(state) {
 					if (Page.serialize) return Page.serialize(state);
 					else return '<!DOCTYPE html>\n' + document.documentElement.outerHTML;
 				}).then(function(doc) {
 					done(null, doc);
-				}).catch(done);
+				}).catch(function(err) {
+					done(err);
+				});
 			});
 		}).then(function(obj) {
 			if (!obj) throw new HttpError.BadRequest("Empty response");
