251f5908e58b39829f6f75efe5677453ca1a7a3c
Jérémy Lal <kapouer@melix.org>
2019-12-10 01:42:50 +0100

    pdf: fake polyfill to avoid useless warnings

--- packages/pdf/lib/pdf.js
+++ packages/pdf/lib/pdf.js
@@ -1,2 +1,10 @@
-exports.plugin = require('express-dom-pdf').plugin;
+exports.plugin = function(page, settings, req, res) {
+	// does not really need a polyfill
+	settings.scripts.push(`window.ResizeObserver = class {
+		disconnect() {}
+		observe() {}
+		unobserve() {}
+	};`);
+	return require('express-dom-pdf').plugin(page, settings, req, res);
+};
 
