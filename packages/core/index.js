6d63d870a76e840ae24dd38d630f2f646c7c93f8
Jérémy Lal <kapouer@melix.org>
2020-01-22 13:28:16 +0100

    Backport toml parser

--- packages/core/index.js
+++ packages/core/index.js
@@ -7,6 +7,7 @@ var morgan = require('morgan');
 var pad = require('pad');
 var prettyBytes = require('pretty-bytes');
 var rc = require('rc');
+var toml = require('toml');
 var mkdirp = pify(require('mkdirp'));
 var xdg = require('xdg-basedir');
 var resolvePkg = require('resolve-pkg');
@@ -59,7 +60,7 @@ exports.config = function(pkgOpt) {
 		},
 		extnames: [],
 		upstreams: {}
-	});
+	}, null, (str) => toml.parse(str));
 	opt.upstream = opt.upstreams[opt.version];
 	if (!opt.port) {
 		if (opt.upstream) opt.port = opt.upstream.split(':').pop();
