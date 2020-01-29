5887a011cd9d3db313a3147c7eafc495bf22c394
Jérémy Lal <kapouer@melix.org>
2020-01-29 12:43:57 +0100

    prerender: do not assume options means select

--- packages/prerender/plugins/form.js
+++ packages/prerender/plugins/form.js
@@ -13,7 +13,7 @@ module.exports = function formPlugin(page) {
 			Array.from(document.forms).forEach(function(form) {
 				Array.from(form.elements).forEach(function(el) {
 					if (el.checked) setAttr(el, 'checked', null);
-					else if (el.options) Array.from(el.options).forEach(function(opt) {
+					else if (el.matches('select') && el.options) Array.from(el.options).forEach(function(opt) {
 						if (opt.selected) setAttr(el, 'selected', null);
 					});
 					else if (el.matches('textarea')) el.innerHTML = el.value;
