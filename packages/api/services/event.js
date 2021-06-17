6c0e4c8f172bfe2f108068ca3889aa0a8dbe7e89
Jérémy Lal <kapouer@melix.org>
2021-06-18 00:40:12 +0200

    event api is actually reservation. Rename methods.

--- packages/api/services/event.js
+++ packages/api/services/event.js
@@ -2,13 +2,14 @@ const { raw, ref } = require('objection');
 
 exports = module.exports = function (opt) {
 	return {
-		name: 'event',
+		name: 'reservation',
 		service: function () { } // FIXME remove me ?
 	};
 };
 
-exports.subscribe = function (req, data) {
-	if (!data.reservation.attendees || data.reservation.attendees.length == 0) {
+exports.add = function (req, data) {
+	const reservation = data.reservation;
+	if (!reservation.attendees || reservation.attendees.length == 0) {
 		throw new HttpError.BadRequest("reservation.attendees must not be empty");
 	}
 	return Promise.all([
@@ -34,72 +35,44 @@ exports.subscribe = function (req, data) {
 			type: 'event_reservation',
 			parent: { parents }
 		}).then(function (obj) {
-			const reservation = data.reservation;
+			if (obj.items.length == 1) {
+				throw HttpError('Conflict', "User already has a reservation for this date", {
+					reservation: obj.items[0].id
+				});
+			}
 			if (reservation.attendees) {
 				reservation.seats = reservation.attendees.length;
 			} else if (reservation.seats == null) {
 				reservation.seats = 1;
 			}
-			const payment = {
-				method: (reservation.payment || {}).method,
-				due: 0,
-				paid: 0
-			};
-			let total = eventDate.data.reservations || 0;
+
+
+			const total = (eventDate.data.reservations || 0) + reservation.seats;
+			if (Number.isNaN(total)) {
+				throw new HttpError.BadRequest("At least one seat must be reserved");
+			}
+
 			if (reservation.seats > 0) {
-				const maxSeatsRes = eventDate.parent.data.maxSeatsReservations;
-				if (maxSeatsRes && reservation.seats > maxSeatsRes) {
-					throw new HttpError.BadRequest("Cannot reserve that much seats at once");
-				}
-				payment.due = (eventDate.data.price || eventDate.parent.data.price || 0) * reservation.seats;
-			}
-
-
-			let blockMeth, resa;
-			if (obj.items.length == 1) {
-				resa = obj.items[0];
-				total += -resa.data.seats;
-				if (resa.data.payment) {
-					payment.paid = resa.data.payment.paid;
-				}
-				blockMeth = 'block.save';
-				resa = {
-					id: resa.id,
-					type: 'event_reservation',
-					data: {
-						seats: reservation.seats,
-						contact: reservation.contact,
-						attendees: reservation.attendees,
-						payment: payment
-					}
-				};
-			} else if (obj.items.length == 0) {
-				blockMeth = 'block.add';
-				resa = {
-					type: 'event_reservation',
-					data: {
-						seats: reservation.seats,
-						contact: reservation.contact,
-						attendees: reservation.attendees,
-						payment: payment
-					},
-					parents: parents,
-					lock: { read: [`id-${req.user.id}`, 'scheduler'] }
-				};
-			} else {
-				console.error("event.subscribe found out multiple subscriptions", reservation);
-				throw new Error("Multiple subscriptions already exists");
-			}
-			total += resa.data.seats;
-			if (Number.isNaN(total)) throw new HttpError.BadRequest("At least one seat must be reserved");
-			if (resa.data.seats > 0) {
 				const maxSeats = eventDate.data.seats || eventDate.parent.data.seats || 0;
 				if (maxSeats > 0 && total > maxSeats) {
 					throw new HttpError.BadRequest("Cannot reserve this number of seats");
 				}
+				const maxSeatsRes = eventDate.parent.data.maxSeatsReservations;
+				if (maxSeatsRes && reservation.seats > maxSeatsRes) {
+					throw new HttpError.BadRequest("Cannot reserve that much seats at once");
+				}
+				reservation.payment.due = (eventDate.data.price || eventDate.parent.data.price || 0) * reservation.seats;
+			} else {
+				reservation.payment.paid = 0;
 			}
+			const resa = {
+				type: 'event_reservation',
+				data: reservation,
+				parents: parents,
+				lock: { read: [`id-${req.user.id}`, 'scheduler'] }
+			};
 
-			return All.run(blockMeth, req, resa).then(function (resa) {
+			return All.run('block.add', req, resa).then(function (resa) {
 				return eventDate.$query(req.trx).patch({
 					'data:reservations': total
 				}).then(function () {
@@ -111,8 +84,8 @@ exports.subscribe = function (req, data) {
 	});
 };
 
-exports.subscribe.schema = {
-	title: 'Subscribe',
+exports.add.schema = {
+	title: 'Add reservation',
 	$action: 'write',
 	required: ['event_date', 'reservation', 'email'],
 	properties: {
@@ -179,9 +152,88 @@ exports.subscribe.schema = {
 		}
 	}
 };
-exports.subscribe.external = true;
+exports.add.external = true;
 
-exports.unsubscribe = function (req, data) {
+exports.save = function (req, data) {
+	const reservation = data.reservation;
+	if (!reservation.attendees || reservation.attendees.length == 0) {
+		throw new HttpError.BadRequest("reservation.attendees must not be empty");
+	}
+	return Promise.all([
+		All.run('settings.find', req, { email: data.email }),
+		All.run('block.find', req, {
+			child: {
+				type: 'event_reservation',
+				id: data.id
+			},
+			children: {
+				type: 'event_reservation',
+				first: true
+			},
+			type: 'event_date',
+			parents: {
+				type: 'event',
+				first: true
+			}
+		})
+	]).then(function ([settings, { item: eventDate }]) {
+		if (req.user.id !== settings.id && !req.user.grants.includes('scheduler')) {
+			throw new HttpError.Unauthorized("Wrong user");
+		}
+		const resa = eventDate.child;
+
+		if (reservation.attendees) {
+			reservation.seats = reservation.attendees.length;
+		} else if (reservation.seats == null) {
+			reservation.seats = 1;
+		}
+		const total = (eventDate.data.reservations || 0) - resa.data.seats + reservation.seats;
+		if (Number.isNaN(total)) {
+			throw new HttpError.BadRequest("At least one seat must be reserved");
+		}
+
+		if (reservation.seats > 0) {
+			const maxSeats = eventDate.data.seats || eventDate.parent.data.seats || 0;
+			if (maxSeats > 0 && total > maxSeats) {
+				throw new HttpError.BadRequest("Cannot reserve this number of seats");
+			}
+			const maxSeatsRes = eventDate.parent.data.maxSeatsReservations;
+			if (maxSeatsRes && reservation.seats > maxSeatsRes) {
+				throw new HttpError.BadRequest("Cannot reserve that much seats at once");
+			}
+			reservation.payment.due = (eventDate.data.price || eventDate.parent.data.price || 0) * reservation.seats;
+		} else {
+			reservation.payment.due = 0;
+		}
+		reservation.payment.paid = resa.data.payment.paid;
+
+		Object.assign(resa.data, reservation);
+
+		return All.run('block.save', req, resa).then(function (resa) {
+			return eventDate.$query(req.trx).patch({
+				'data:reservations': total
+			}).then(function () {
+				return resa;
+			});
+		});
+	});
+};
+exports.save.schema = Object.assign({}, exports.add.schema, {
+	title: 'Save reservation',
+	required: ['id', 'reservation', 'email']
+});
+Object.assign({}, exports.save.schema.properties, {
+	event_date: undefined,
+	id: {
+		title: 'Reservation id',
+		type: 'string',
+		format: 'id'
+	}
+});
+
+exports.save.external = true;
+
+exports.del = function (req, data) {
 	return All.block.get(req, {
 		type: 'event_reservation',
 		id: data.reservation
@@ -210,8 +262,8 @@ exports.unsubscribe = function (req, data) {
 		});
 	});
 };
-exports.unsubscribe.schema = {
-	title: 'Unsubscribe',
+exports.del.schema = {
+	title: 'Delete reservation',
 	$action: 'write',
 	required: ['reservation'],
 	properties: {
@@ -222,7 +274,7 @@ exports.unsubscribe.schema = {
 		}
 	}
 };
-exports.unsubscribe.external = true;
+exports.del.external = true;
 
 
 exports.pay = function (req, data) {
@@ -240,7 +292,7 @@ exports.pay = function (req, data) {
 	});
 };
 exports.pay.schema = {
-	title: 'Pay',
+	title: 'Pay reservation',
 	$action: 'write',
 	required: ['reservation'],
 	properties: {
@@ -259,7 +311,7 @@ exports.pay.schema = {
 };
 exports.pay.external = true;
 
-exports.reservations = function ({ site, trx }, data) {
+exports.search = function ({ site, trx }, data) {
 	// given an event_date, retrieve reservations, user settings and email
 	return site.$relatedQuery('children', trx)
 		.where('block.type', 'event_date')
@@ -304,8 +356,8 @@ exports.reservations = function ({ site, trx }, data) {
 			return { item: eventDate };
 		});
 };
-exports.reservations.schema = {
-	title: 'List reservations',
+exports.search.schema = {
+	title: 'Search reservations',
 	$action: 'read',
 	required: ['id'],
 	properties: {
@@ -330,4 +382,4 @@ exports.reservations.schema = {
 		}
 	}
 };
-exports.reservations.external = true;
+exports.search.external = true;
