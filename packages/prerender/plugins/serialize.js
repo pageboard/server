a0628098d135544819dc3601079a10bf0e8d0568
Jérémy Lal <kapouer@melix.org>
2021-09-06 11:37:39 +0200

    Use Page.finish()

--- packages/prerender/plugins/serialize.js
+++ packages/prerender/plugins/serialize.js
@@ -1,21 +1,18 @@
 module.exports = function(page, settings, request, response) {
 	page.when('idle', function() {
 		if (settings.output == null) return page.run(function(done) {
-			// could use Page.finish().then() the day all clients use window-page@9.2.0
 			/* global Page */
-			Page.init(function(state) {
-				state.queue.then(function(state) {
-					if (Page.serialize) return Page.serialize(state);
-					else return {
-						mime: "text/html",
-						body: '<!DOCTYPE html>\n' + document.documentElement.outerHTML
-					};
-				}).then(function(doc) {
-					done(null, doc);
-				}).catch(function(err) {
-					console.error(err); // FIXME else nobody can actually see the error
-					done(err);
-				});
+			Page.finish().then((state) => {
+				if (Page.serialize) return Page.serialize(state);
+				else return {
+					mime: "text/html",
+					body: '<!DOCTYPE html>\n' + document.documentElement.outerHTML
+				};
+			}).then(function (doc) {
+				done(null, doc);
+			}).catch(function (err) {
+				console.error(err); // FIXME else nobody can actually see the error
+				done(err);
 			});
 		}).then(function(obj) {
 			if (!obj) throw new HttpError.BadRequest("Empty response");
