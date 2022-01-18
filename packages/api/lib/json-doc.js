b32d7138a4bb66bf3ef425172bc737bb1a5f96a9
Jérémy Lal <kapouer@melix.org>
2022-01-18 14:39:23 +0100

    json-doc: show default value

--- packages/api/lib/json-doc.js
+++ packages/api/lib/json-doc.js
@@ -16,6 +16,7 @@ module.exports = function(opt, schema) {
 			if (typeof type != "string") type = 'object';
 			else if (type == "object" && schema.properties) type = "";
 			const path = pointer.split('/').slice(1).filter((x) => x != 'properties').join('.');
+			if (schema.default) type += `|${JSON.stringify(schema.default)}`;
 			lines.push([path + (required ? ' *' : ''), type, schema.title || '-']);
 		}
 	}
