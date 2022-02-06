35b0dcd9ca2c292e41bb0a8357e45d2e6df096f8
Jérémy Lal <kapouer@melix.org>
2022-02-07 00:41:47 +0100

    api "page" format: which url can be served as documents

--- packages/api/index.js
+++ packages/api/index.js
@@ -22,6 +22,7 @@ const ajvApiSettings = {
 	formats: {
 		singleline: /^[^\n\r]*$/,
 		pathname: /^(\/[\w-.]*)+$/,
+		page: /^((\/[a-zA-Z0-9-]*)+)$|^(\/\.well-known\/\d{3})$/,
 		id: /^[A-Za-z0-9]+$/,
 		name: /^\w+$/, // this should be the "type" format
 		grant: /^[a-z0-9-]+$/ // this should be the name format !
