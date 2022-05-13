a0882647fe411e7794c6e2809b5813c72370eb2d
Jérémy Lal <kapouer@melix.org>
2022-05-13 10:57:57 +0200

    api: queryBuilder safer check for date slot

--- packages/api/models/common.js
+++ packages/api/models/common.js
@@ -326,7 +326,7 @@ function asPaths(obj, ret, pre, first, schema) {
 		} else {
 			cur = key;
 		}
-		if (val && schem.type == "object" && Object.keys(schem.properties).join(' ') == "start end" && dateTimes.includes(schem.properties.start.format) && dateTimes.includes(schem.properties.end.format)) {
+		if (val && schem.type == "object" && Object.keys(schem.properties).sort().join(' ') == "end start" && dateTimes.includes(schem.properties.start.format) && dateTimes.includes(schem.properties.end.format)) {
 			// we have a date slot
 			const range = dateRange(val);
 			if (range) {
