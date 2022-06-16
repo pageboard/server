c1ee207415d4792d3e4814a7a8be4f2d1a9bd825
Jérémy Lal <kapouer@melix.org>
2022-06-16 12:21:22 +0200

    More block tests

--- tests/block.test.js
+++ tests/block.test.js
@@ -29,4 +29,44 @@ suite('block', function () {
 		assert.equal('_id' in JSON.parse(JSON.stringify(b)), false);
 	});
 
+	test('add block: validation for missing property', async function () {
+		assert.rejects(app.run('block.add', {
+			type: 'api_form', data: {}
+		}, 'test'), {
+			name: 'ValidationError',
+			message: "data.action: must have required property 'action'"
+		});
+	});
+
+	test('save block', async function () {
+		const b1 = await app.run('block.add', {
+			type: 'page', data: { url: '/test' }
+		}, 'test');
+
+		const b2 = await app.run('block.save', {
+			id: b1.id,
+			type: 'page',
+			data: { url: '/test2' }
+		}, 'test');
+		assert.equal(b2.id, b1.id);
+		assert.equal(b2.data.url, '/test2');
+	});
+
+	test('delete block', async function () {
+		const b1 = await app.run('block.add', {
+			type: 'page', data: { url: '/test' }
+		}, 'test');
+
+		const b2 = await app.run('block.del', {
+			id: b1.id,
+			type: 'page'
+		}, 'test');
+
+		assert.deepEqual(b2, { count: 1 });
+
+		assert.rejects(app.run('block.get', { id: b1.id }), {
+			name: 'BlockNotFound'
+		});
+	});
+
 });
