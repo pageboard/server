454b312597671329d6614ca03d6d56607ece3aa2
Jérémy Lal <kapouer@melix.org>
2022-07-06 16:57:40 +0200

    page.test: test match page

--- tests/page.test.js
+++ tests/page.test.js
@@ -38,16 +38,48 @@ suite('page', function () {
 		const b = await app.run('page.add', {
 			type: 'page', data: { url: '/test/a' }
 		}, 'test');
-		const c = await app.run('page.get', {
-			url: '/test/a'
-		}, 'test');
 		assert.ok('id' in b, 'has id');
 		assert.equal(typeof b.updated_at, "string");
 		assert.deepEqual(Object.keys(b), ["id", "updated_at"]);
-		assert.equal(c.id, b.id);
-		assert.equal(c.updated_at, b.udpated_at);
-		assert.equal(c.type, 'page');
-		assert.equal(c.data.url, '/test/a');
+		const result = await app.run('page.get', {
+			url: '/test/a'
+		}, 'test');
+		const { item, links, status, site, meta } = result;
+		assert.ok(item);
+		assert.ok(site);
+		assert.ok(meta);
+		assert.equal(meta.name, 'page');
+		assert.equal(status, 200);
+		assert.deepEqual(links, { up: [] });
+		assert.equal(item.id, b.id);
+		assert.equal(item.updated_at, b.updated_at);
+		assert.equal(item.type, 'page');
+		assert.equal(item.data.url, '/test/a');
+	});
+
+	test('page match', async function () {
+		await app.run('page.add', {
+			type: 'page', data: { url: '/test/special' }
+		}, 'test');
+
+		await app.run('page.add', {
+			type: 'page', data: { url: '/test', match: true }
+		}, 'test');
+
+		const gen = await app.run('page.get', {
+			url: '/test/generic'
+		}, 'test');
+		assert.equal(gen.status, 200);
+		assert.equal(gen.item.data.match, true);
+		assert.equal(gen.item.data.url, '/test');
+
+		const spe = await app.run('page.get', {
+			url: '/test/special'
+		}, 'test');
+		assert.equal(spe.status, 200);
+		assert.equal(spe.item.data.match, false);
+		assert.equal(spe.item.data.url, '/test/special');
+
 	});
 
 
