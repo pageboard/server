cd51b55cd257c42473e9e507a0d6fadf157f839c
Jérémy Lal <kapouer@melix.org>
2022-09-19 14:33:56 +0200

    auth.login: fix call

--- packages/auth/services/login.js
+++ packages/auth/services/login.js
@@ -199,7 +199,7 @@ module.exports = class LoginModule {
 	};
 
 	async link(req, data) {
-		const token = await this.generate(req, data);
+		const token = await this.#generate(req, data);
 		return "/.api/login?" + new URLSearchParams({
 			email: data.email,
 			grant: 'webmaster',
