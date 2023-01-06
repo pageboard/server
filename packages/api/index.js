2e8728ced54777fb918866ef68b2429734bac87b
Jérémy Lal <kapouer@melix.org>
2023-01-06 15:14:54 +0100

    Some root elements can be without bundle

--- packages/api/index.js
+++ packages/api/index.js
@@ -211,18 +211,14 @@ module.exports = class ApiModule {
 			if (obj.item) fillTypes(obj.item, usedTypes);
 			if (obj.items) fillTypes(obj.items, usedTypes);
 
-			// on parcourt tous les usedTypes
-			// et on compte les bundles qui les contiennent
-			// s'il n'y a qu'un bundle c'est lui qu'on prend
-			// s'il y a le choix, on prend le bundle qui contient le plus de usedTypes
 			const usedRoots = new Set();
 			for (const type of usedTypes) {
 				if (bundles[type]) {
 					usedRoots.add(type);
 				} else {
 					const rootSet = bundleMap.get(type);
-					// trick: ignore elements belonging to multiple roots
-					if (rootSet.size > 1) continue;
+					// ignore elements without bundles, or belonging to multiple roots
+					if (rootSet.size != 1) continue;
 					usedRoots.add(Array.from(rootSet).at(0));
 				}
 			}
