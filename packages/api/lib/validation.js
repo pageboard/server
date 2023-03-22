0f15685e1cfe231ad7536886d59e8daa58478d3f
Jérémy Lal <kapouer@melix.org>
2023-03-23 00:38:26 +0100

    Add hex-color format

--- packages/api/lib/validation.js
+++ packages/api/lib/validation.js
@@ -54,6 +54,7 @@ module.exports = class Validation {
 		formats: {
 			singleline: /^[^\n\r]*$/,
 			pathname: /^(\/[\w.-]*)+$/,
+			'hex-color': /^(#[0-9A-Fa-f]{6}([0-9A-Fa-f]{2})?|\w+)$/,
 			page: /^((\/[a-zA-Z0-9-]*)+)$|^(\/\.well-known\/\d{3})$/,
 			id: /^[A-Za-z0-9]+$/,
 			name: /^\w+$/, // this should be the "type" format
