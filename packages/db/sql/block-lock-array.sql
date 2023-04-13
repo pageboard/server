d2346a89618fe3b11f4fcf9dd7e64e915c7614a4
Jérémy Lal <kapouer@melix.org>
2023-04-13 16:52:26 +0200

    Migrate block.lock back to array type

--- packages/db/sql/block-lock-array.sql
+++ packages/db/sql/block-lock-array.sql
@@ -1,2 +1,9 @@
+
 UPDATE block SET lock = block.lock['read'] WHERE block.lock['read'] IS NOT NULL;
-UPDATE block SET lock = null WHERE lock = '[]' OR lock = '{}';
+UPDATE block SET lock = block.lock['write'] WHERE block.lock['write'] IS NOT NULL;
+UPDATE block SET lock = NULL WHERE lock = '[]' OR lock = '{}' OR lock = 'null';
+
+ALTER TABLE block RENAME COLUMN lock TO jlock;
+ALTER TABLE block ADD COLUMN lock TEXT[];
+UPDATE block SET lock = ARRAY(SELECT jsonb_array_elements_text(jlock)) WHERE jlock IS NOT NULL;
+ALTER TABLE block DROP COLUMN jlock;
