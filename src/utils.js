ac91c709d7ba62066a670e0341626f6ef0837130
Jérémy Lal <kapouer@melix.org>
2023-04-26 16:22:39 +0200

    Use fixed nestie for now

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const { nestie } = require.lazy('nestie');
+// const { nestie } = require.lazy('nestie');
 const { flattie } = require.lazy('flattie');
 const dget = require.lazy('dlv');
 const {
@@ -68,3 +68,45 @@ function dset(obj, keys, val) {
 		t = t[k] = (i === l) ? val : ((x = t[k]) && typeof (x) === typeof (keys)) ? x : (k = '+' + keys[i]) * 0 !== 0 || /\./.test(k) ? {} : [];
 	}
 }
+
+function empty(key) {
+	if (key == null || key === "") return {};
+	key = +key;
+	return key === key ? [] : {};
+}
+
+function nestie(input, glue) {
+	glue = glue || '.';
+	var arr, tmp, output;
+	var i = 0, k, key;
+
+	for (k in input) {
+		tmp = output; // reset
+		arr = k.split(glue);
+
+		for (i = 0; i < arr.length;) {
+			key = arr[i++];
+
+			if (tmp == null) {
+				tmp = empty(key);
+				output = output || tmp;
+			}
+
+			if (key == '__proto__' || key == 'constructor' || key == 'prototype') break;
+
+			if (i < arr.length) {
+				if (key in tmp) {
+					tmp = tmp[key];
+				} else {
+					tmp = tmp[key] = empty(arr[i]);
+				}
+			} else {
+				tmp[key] = input[k];
+			}
+		}
+	}
+
+	return output;
+}
+
+exports.nestie = nestie;
