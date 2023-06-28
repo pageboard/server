66a7330e13b988f393eddb939588cc3beec237ad
Jérémy Lal <kapouer@melix.org>
2023-06-28 23:43:16 +0200

    auth login: post expect body

--- packages/auth/services/login.js
+++ packages/auth/services/login.js
@@ -13,12 +13,12 @@ module.exports = class LoginModule {
 	}
 	apiRoutes(app, server) {
 		server.post("/.api/login/send", async (req, res) => {
-			const data = await req.run('login.send', req.query);
+			const data = await req.run('login.send', req.body);
 			res.return(data);
 		});
 
 		server.post("/.api/login/grant", async (req, res) => {
-			const data = await req.run('login.grant', req.query);
+			const data = await req.run('login.grant', req.body);
 			res.return(data);
 		});
 
@@ -297,7 +297,7 @@ module.exports = class LoginModule {
 				errorCorrectionLevel: 'L'
 			});
 			return { item };
-		}		
+		}
 	}
 	static key = {
 		title: 'Get user otp key',
