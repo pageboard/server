07bcbc2d67f41d888fdb4e36b4cd153df3c613bc
Jérémy Lal <kapouer@melix.org>
2023-07-19 02:26:11 +0200

    print.remote api converging

--- packages/print/index.js
+++ packages/print/index.js
@@ -14,6 +14,11 @@ module.exports = class PrintModule {
 		this.opts = opts;
 	}
 
+	async init() {
+		const { withCache } = await import("ultrafetch");
+		this.opts.fetch = withCache(fetch);
+	}
+
 	async list(req) {
 		const list = await cups.getPrinterNames();
 		if (this.opts.storage) {
@@ -122,10 +127,12 @@ module.exports = class PrintModule {
 		}
 	};
 
-	async remote(req, { printer, path }) {
+	async remote(req, {
+		url, printer, options = {}, delivery = {}
+	}) {
 		const { expresta: conf } = this.opts;
 		if (!conf) throw new HttpError.NotFound("No remote printer");
-		const agent = new BearerAgent(conf.url);
+		const agent = new BearerAgent(this.opts, conf.url);
 
 		agent.bearer = (await agent.fetch("/login", "post", {
 			email: conf.email,
@@ -133,33 +140,206 @@ module.exports = class PrintModule {
 		})).token;
 
 		const products = await agent.fetch("/data/products");
-		const product = products.find(item => item.url == 'booklet-printing');
-		if (!product) throw new HttpError.NotFound("No product with that name");
-		return { workinprogress: true };
+		const product = products.find(item => item.url == options.product);
+		if (!product) {
+			return {
+				status: 400,
+				statusText: 'Unknown product',
+				items: products
+			};
+		}
+
+		const papers = await agent.fetch("/data/papers");
+		const paper = papers.find(item => item.id == options.paper);
+		if (!paper) {
+			return {
+				status: 400,
+				statusText: 'Unknown paper',
+				items: papers
+			};
+		}
+
+		const folds = await agent.fetch("/data/folds");
+		const fold = folds.find(item => item.id == options.fold);
+		if (!fold && options.fold) {
+			return {
+				status: 400,
+				statusText: 'Unknown fold',
+				items: folds
+			};
+		}
+
+		const bindings = await agent.fetch("/data/bindings");
+		const binding = bindings.find(item => item.id == options.binding);
+		if (!binding) {
+			return {
+				status: 400,
+				statusText: 'Unknown binding',
+				items: bindings
+			};
+		}
+
+		const couriers = await agent.fetch(`/data/deliveries-by-courier/${delivery.iso_code}`);
+		const courier = couriers.find(item => item.courier == delivery.courier);
+		if (!courier) {
+			return {
+				status: 400,
+				statusText: 'Unknown courier',
+				items: couriers
+			};
+		}
+
+		const pdfUrl = new URL(url, req.site.url);
+		// 1. find pdf item
+		const { item: pdf } = await req.run('block.find', {
+			type: 'pdf',
+			data: {
+				url: pdfUrl.pathname.replace(/\.pdf$/, '')
+			}
+		});
+		if (!pdf) throw new HttpError.NotFound('PDF not found');
+		pdfUrl.searchParams.set('pdf', 'printer');
+		const pdfPaper = pdf.data.paper;
+		const sizeA = convertLengthToMillimiters(pdfPaper.width) || 210;
+		const sizeB = convertLengthToMillimiters(pdfPaper.height) || 297;
+		const margin = convertLengthToMillimiters(pdfPaper.margin) || 0;
+		if (margin > 0 && margin < 5) {
+			throw new HttpError.BadRequest("Margin should be >= 5mm for bleed to work");
+		}
+
+
+		// https://api.expresta.com/api/v1/order/sandbox-create for testing orders
+
+
+		const order = {
+			customer_reference: "1234567890",
+			delivery,
+			products: [{
+				product_type_id: product.id, // GetProducts id
+				pdf: pdfUrl.href,
+				binding_id: binding.id,
+				binding_placement: "left",
+				amount: 1,
+				runlists: [{
+					size_a: sizeA,
+					size_b: sizeB,
+					paper_id: paper.id,
+					bleed: !margin,
+					fold_type_id: fold?.id,
+					fold_on: options.fold_on
+				}]
+			}]
+		};
+		//const price = await agent.fetch("/order/calculate-price", "post", {data: order});
+		const ret = await agent.fetch("/order/sandbox-create", "post", { data: order });
+		return {
+			type: 'order',
+			data: {
+				order
+				//price
+			},
+			status: ret.status == "error" ? 500 : 200,
+			statusText: ret.msg
+		};
 	}
 	static remote = {
 		title: 'Remote print',
 		$action: 'write',
+		required: ['url', 'delivery', 'printer', 'options'],
 		properties: {
-			provider: {
-				title: 'Provider',
-				description: 'Choose a supported provider',
+			url: {
+				title: 'PDF page',
+				type: "string",
+				format: "uri-reference",
+				$filter: {
+					name: 'helper',
+					helper: {
+						name: 'page',
+						type: 'pdf'
+					}
+				},
+				$helper: 'href'
+			},
+			printer: {
+				title: 'Printer service',
+				description: 'Choose a supported printer service',
 				anyOf: [{ const: 'expresta', title: 'Expresta' }]
 			},
-			product: {
-				title: 'Product',
-				type: 'string',
-				format: 'singleline'
+			options: {
+				title: 'Print options',
+				type: 'object',
+				//required: ['product', 'paper', 'size'],
+				properties: {
+					product: {
+						title: 'Product',
+						type: 'string',
+						format: 'singleline'
+					},
+					paper: {
+						title: 'Paper',
+						type: 'string',
+						format: 'id',
+						default: '420'
+					},
+					fold: {
+						title: 'Fold',
+						type: 'string',
+						format: 'id',
+						default: '19'
+					},
+					fold_on: {
+						title: 'Fold on',
+						default: null,
+						anyOf: [{
+							type: 'null',
+							title: 'n/a'
+						}, {
+							const: 'axis_longer',
+							title: 'Long side'
+						}, {
+							const: 'axis_longer',
+							title: 'Short side'
+						}]
+					},
+					binding: {
+						title: 'Binding',
+						type: 'string',
+						format: 'id',
+						default: '254' // paperback, perfect binding
+					}
+				}
 			},
-			paper: {
-				title: 'Paper',
-				type: 'string',
-				format: 'singleline'
-			},
-			url: {
-				title: 'URL',
-				type: 'string',
-				format: 'uri'
+			delivery: {
+				title: 'Delivery',
+				type: 'object',
+				//required: ['iso_code', 'name', 'phone', 'email', 'street', 'city', 'zip'],
+				properties: {
+					courier: {
+						title: 'Courier' // as obtained by GetDeliveriesByCourier
+
+					},
+					iso_code: {
+						title: 'Country Code' // the same used to call GetDeliveriesByCourier
+					},
+					name: {
+
+					},
+					phone: {
+
+					},
+					email: {
+
+					},
+					street: {
+
+					},
+					city: {
+
+					},
+					zip: {
+
+					}
+				}
 			}
 		}
 	};
@@ -201,3 +381,12 @@ module.exports = class PrintModule {
 		}
 	}
 };
+
+
+function convertLengthToMillimiters(str = '') {
+	const num = parseFloat(str);
+	if (Number.isNaN(num)) return 0;
+	const { groups: { unit } } = /\d+(?<unit>\w+)/.exec(str) ?? { groups: {} };
+	if (unit == "cm") return num * 10;
+	else if (unit == "mm") return num;
+}
--- packages/print/src/agent.js
+++ packages/print/src/agent.js
@@ -1,18 +1,25 @@
 module.exports = class BearerAgent {
-	constructor(baseUrl) {
+
+	constructor(opts, baseUrl) {
 		this.baseUrl = baseUrl;
+		this.opts = opts;
 	}
+
 	async fetch(path, method = "get", query = {}) {
 		const opts = {};
 		if (!Object.isEmpty(query)) {
 			const fd = new FormData();
 			for (const [key, val] of Object.entries(query)) {
-				fd.append(key, val);
+				if (val != null && typeof val == "object") {
+					fd.append(key, JSON.stringify(val));
+				} else {
+					fd.append(key, val);
+				}
 			}
 			opts.body = fd;
 		}
 		if (this.bearer) opts.headers = { Authorization: `Bearer ${this.bearer}` };
-		const res = await fetch(this.baseUrl + path, {
+		const res = await this.opts.fetch(this.baseUrl + path, {
 			method,
 			redirect: 'follow',
 			...opts
