9c18bc5723dbdef73fdf537fc6909244377e7d67
Jérémy Lal <kapouer@melix.org>
2023-10-11 12:08:29 +0200

    Change lang format, add ext format

--- packages/api/lib/formats.js
+++ packages/api/lib/formats.js
@@ -6,6 +6,7 @@ module.exports = {
 	id: /^[A-Za-z0-9]+$/,
 	name: /^\w*$/, // this should be the "type" format
 	grant: /^[a-z0-9-]+$/, // this should be the name format !
-	lang: /^([a-zA-Z]+-?)+$/,
+	lang: /^[a-z]{2}$/,
+	ext: /^[a-z]{3,4}$/,
 	phone: /^[+(\s.\-/\d)]{5,30}$/
 };
