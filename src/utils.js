c87f39965aadb85e28d3bf06e8ced4b77d37009f
Jérémy Lal <kapouer@melix.org>
2023-10-20 14:20:24 +0200

    matchdom update

--- src/utils.js
+++ src/utils.js
@@ -2,28 +2,36 @@
 const { flattie } = require.lazy('flattie');
 const dget = require.lazy('dlv');
 const {
-	Matchdom, TextPlugin, ArrayPlugin, OpsPlugin, NumPlugin, DatePlugin
+	Matchdom, TextPlugin, ArrayPlugin, OpsPlugin, NumPlugin, DatePlugin, RepeatPlugin
 } = require('../lib/matchdom');
 const getSlug = require.lazy('speakingurl');
 const { access } = require('node:fs/promises');
 
-const sharedMd = new Matchdom(TextPlugin, ArrayPlugin, OpsPlugin, NumPlugin, DatePlugin, {
-	formats: {
-		as: {
-			slug: (ctx, str) => getSlug(str, { custom: { "_": "-" } }),
-			query: (ctx, obj) => {
-				if (!obj) return obj;
-				const q = new URLSearchParams();
-				for (const [key, val] of Object.entries(obj)) {
-					if (Array.isArray(val)) for (const item of val) q.append(key, item);
-					else if (val !== null) q.append(key, val);
+const sharedMd = new Matchdom(
+	TextPlugin,
+	ArrayPlugin,
+	OpsPlugin,
+	NumPlugin,
+	DatePlugin,
+	RepeatPlugin,
+	{
+		formats: {
+			as: {
+				slug: (ctx, str) => getSlug(str, { custom: { "_": "-" } }),
+				query: (ctx, obj) => {
+					if (!obj) return obj;
+					const q = new URLSearchParams();
+					for (const [key, val] of Object.entries(obj)) {
+						if (Array.isArray(val)) for (const item of val) q.append(key, item);
+						else if (val !== null) q.append(key, val);
+					}
+					const ser = q.toString();
+					return ser ? `?${ser}` : '';
 				}
-				const ser = q.toString();
-				return ser ? `?${ser}` : '';
 			}
 		}
 	}
-});
+);
 
 exports.dget = dget;
 exports.dset = dset;
