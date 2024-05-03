0f40811263d5d77189d575b5923db498117fffc6
Jérémy Lal <kapouer@melix.org>
2024-05-03 15:07:55 +0200

    kqueue: simplify

--- src/kqueue.js
+++ src/kqueue.js
@@ -1,24 +1,17 @@
-const { Deferred } = require.lazy('class-deferred');
-
 module.exports = class KQueue {
 	#map = new Map();
 	push(key, fn) {
-		const d = new Deferred();
 		const obj = this.#map.get(key) ?? { p: Promise.resolve() };
 		if (!obj.count) {
 			obj.count = 0;
 			this.#map.set(key, obj);
 		}
 		obj.count++;
-		obj.p = obj.p.then(fn).then(res => {
-			d.resolve(res);
-		}).catch (err => {
-			d.reject(err);
-		}).finally(() => {
+		obj.p = obj.p.then(fn);
+		return obj.p.finally(() => {
 			if (--obj.count == 0) {
 				this.#map.delete(key);
 			}
 		});
-		return d;
 	}
 };
