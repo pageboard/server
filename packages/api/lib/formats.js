1eae96a0544e04b8472e54ea0a3dfdaf7c285c71
Jérémy Lal <kapouer@melix.org>
2024-06-13 14:24:11 +0200

    validation: name should allow dash

--- packages/api/lib/formats.js
+++ packages/api/lib/formats.js
@@ -4,7 +4,7 @@ module.exports = {
 	page: /^((\/[a-zA-Z0-9-]*)+)$|^(\/\.well-known\/\d{3})$/, // only for pages url
 	'hex-color': /^(#[0-9A-Fa-f]{6}([0-9A-Fa-f]{2})?|\w+)$/,
 	id: /^[A-Za-z0-9]+$/,
-	name: /^\w*$/, // this should be the "type" format
+	name: /^[-\w]*$/, // this should be the "type" format
 	grant: /^[a-z0-9-]+$/, // this should be the name format !
 	lang: /^[a-z]{2}$/,
 	ext: /^[a-z]{3,4}$/,
