a8433189405b638d373a0d3ba26b0335b8c8f483
Jérémy Lal <kapouer@melix.org>
2024-06-20 14:58:05 +0200

    tests: auth test

---
+++ tests/auth.test.js
@@ -0,0 +1,35 @@
+const assert = require('node:assert');
+const { site, app, setupHelper } = require('./helpers/common');
+
+suite('auth', function () {
+
+	this.timeout(require('node:inspector').url() === undefined ? 10000 : 0);
+
+	suiteSetup(setupHelper);
+
+	test('grant, get token, verify it', async function () {
+		const email = 'test@test.localhost.localdomain';
+		const grant = 'webmaster';
+
+		await app.run('settings.grant', {
+			email, grant
+		}, { site: site.id, grant: 'root' });
+
+		const token = new URLSearchParams((await app.run('login.link', {
+			email, grant
+		}, { site: site.id, grant: 'root' })).split('?').pop()).get('token');
+
+		const { item, cookies } = await app.run('login.grant', {
+			email, grant, token
+		}, { site: site.id, grant: 'root' });
+
+		assert.ok(cookies.bearer.value);
+		assert.ok(cookies.bearer.maxAge);
+		assert.deepEqual(item.data.grants, [grant]);
+
+	});
+
+
+
+
+});
