06e3fe77dd2421cc5f0ee6b248c3c72dd3d8c257
Jérémy Lal <kapouer@melix.org>
2024-06-26 18:23:42 +0200

    Migrate print_job

---
+++ packages/api/upgrades/from-0.14-to-0.15.js
@@ -0,0 +1,12 @@
+exports.print_job = function ({ data }) {
+	if (!data.device || !data.printer) {
+		if (data.options?.cover.url) {
+			data.device = 'printer';
+			data.printer = 'remote';
+		} else if (data.printer == "storage") {
+			data.device = 'printer';
+			data.printer = 'offline';
+		}
+	}
+
+};
