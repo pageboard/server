4bf6482f50d21150dc8021ec77ba89ab8ce7f939
Jérémy Lal <kapouer@melix.org>
2024-06-26 18:27:17 +0200

    Convert pdf paper too

--- packages/api/upgrades/from-0.14-to-0.15.js
+++ packages/api/upgrades/from-0.14-to-0.15.js
@@ -8,5 +8,17 @@ exports.print_job = function ({ data }) {
 			data.printer = 'offline';
 		}
 	}
+};
 
+exports.mail_job = function ({ data }) {
+	// TODO some mail_job should become print_job
+};
+
+exports.pdf = function ({ data }) {
+	if (!data.paper) return;
+	if (data.paper.preset == "screen") delete data.paper.preset;
+	if (data.paper.width) data.paper.width = parseFloat(data.paper.width);
+	if (data.paper.height) data.paper.height = parseFloat(data.paper.height);
+	if (data.paper.margin) data.paper.margin = parseFloat(data.paper.margin);
+	if (data.paper.spine) data.paper.margin = parseFloat(data.paper.spine);
 };
