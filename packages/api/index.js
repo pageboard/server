e1c9fe78dd6fd5ec0d87c6500bde3c4a3cabab46
Jérémy Lal <kapouer@melix.org>
2024-07-11 11:24:34 +0200

    Just check schema.$action inconsistency

--- packages/api/index.js
+++ packages/api/index.js
@@ -219,12 +219,8 @@ module.exports = class ApiModule {
 					if (schema.$action != "write") {
 						console.warn(method, "had write transaction with $action", schema.$action);
 					}
-				} else if (hadRead) {
-					if (!['read', 'write'].includes(schema.$action)) {
-						console.warn(method, "had read transaction with $action", schema.$action);
-					}
-				} else {
-					console.warn(method, "had no transaction with $action", schema.$action);
+				} else if (hadRead && !schema.$action) {
+					console.warn(method, "had read transaction without $action");
 				}
 			}
 		}
