648632378dec7322d5cdec71f39ca2ae38507b9a
Jérémy Lal <kapouer@melix.org>
2024-09-25 23:45:23 +0200

    plugin elements can change previous ones

--- src/pageboard.js
+++ src/pageboard.js
@@ -420,7 +420,7 @@ module.exports = class Pageboard {
 
 		if (!type) for (const plugin of this.#plugins) {
 			if (typeof plugin.elements == "function") Object.assign(
-				this.elements, await plugin.elements()
+				this.elements, await plugin.elements(this.elements)
 			);
 		}
 
