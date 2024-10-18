780edd44fb1964d0f17a45423dd6746d0f5b5e74
Jérémy Lal <kapouer@melix.org>
2024-10-18 11:15:19 +0200

    reprint: return { item }

--- packages/print/index.js
+++ packages/print/index.js
@@ -158,7 +158,7 @@ module.exports = class PrintModule {
 		}[block.data.printer];
 
 		await req.try(block, (req, block) => job.call(this, req, block));
-		return block;
+		return { item: block };
 	}
 	static again = {
 		title: 'Reprint',
