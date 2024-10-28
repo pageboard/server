72e0698eb6626edf4e1d49df0bdf3b6a635f3b25
Jérémy Lal <kapouer@melix.org>
2024-10-28 16:19:16 +0100

    stat module

--- packages/api/services/stat.js
+++ packages/api/services/stat.js
@@ -1,26 +1,35 @@
 module.exports = class StatService {
 	static name = 'stat';
 
-	async inc({ site, sql: { trx, raw, ref, fun }}, data) {
+	apiRoutes(router) {
+		router.write('/stat/beacons', 'stat.beacons');
+	}
+
+	async beacons({ site, sql: { trx, raw, ref, fun } }, { names }) {
+		if (names.length == 0) throw new HttpError.BadRequest("Empty names");
 		const ret = await site.$relatedQuery('children', trx)
 			.where('type', 'statistic')
-			.where(ref('data:name').castText(), data.name)
+			.whereIn(ref('data:name').castText(), names)
 			.patch({
 				'type': 'statistic',
 				'data:value': raw('?? + ?', [fun('coalesce', ref('data:value').castInt(), 0), 1]),
 			});
-		if (ret != 1) throw new HttpError.NotFound();
+		if (ret != names.length) throw new HttpError.NotFound(names.join(", "));
 		return {};
 	}
-	static inc = {
-		title: 'Increment',
+
+	static beacons = {
+		title: 'Beacons',
 		$action: 'write',
-		required: ['name'],
+		required: ['names'],
 		properties: {
-			name: {
-				title: 'Name of stat',
-				type: 'string',
-				format: 'name'
+			names: {
+				title: 'Names of stats to increment',
+				type: 'array',
+				items: {
+					type: 'string',
+					format: 'singleline'
+				}
 			}
 		}
 	};
