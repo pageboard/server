f2db18135619d796defd6e23752f2f8f17cbd903
Jérémy Lal <kapouer@melix.org>
2025-06-17 12:21:14 +0200

    extend grant format

--- packages/api/lib/formats.js
+++ packages/api/lib/formats.js
@@ -6,7 +6,7 @@ module.exports = {
 	'hex-color': /^(#[0-9A-Fa-f]{6}([0-9A-Fa-f]{2})?|\w+)$/,
 	id: /^[A-Za-z0-9]+$/,
 	name: /^[-\w]*$/, // this should be the "type" format
-	grant: /^[a-z0-9]+(-[A-Za-z0-9]+)?$/, // this should be the name format !
+	grant: /^[a-z0-9]+(-[A-Za-z0-9]+)*$/, // this should be the name format !
 	lang: /^[a-z]{2}(-[a-z]{2})?$/,
 	ext: /^[a-z]{3,4}$/,
 	phone: /^[+(\s.\-/\d)]{5,30}$/,
